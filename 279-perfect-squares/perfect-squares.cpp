0853d5d0b2cadd221fc1d05da456a5cc29b7a3bf
Aditya singh chauhan <adityaschauhan346@gmail.com>
2023-09-09 16:57:58 +0530

    Time: 102 ms (85.81%) | Memory: 9 MB (75.65%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int solve(int n, vector<int>&dp)
+    int solve(int n)
     {
-        if (n == 0) return 0;
-        if (dp[n] != -1) return dp[n];
-        
-        int mnCount = INT_MAX;
-        for (int num = 1; num <= sqrt(n); num++)
+        vector<int>dp(n + 1, INT_MAX);
+        dp[0] = 0; 
+        for (int target = 1; target <= n; target++)
         {
-            int sqNum = num * num;
-            int currCount = 1 + solve(n - sqNum, dp);
-            mnCount = min(mnCount, currCount);
+            int mnCount = INT_MAX;
+            for (int num = 1; num <= sqrt(target); num++)
+            {
+                int sqNum = num * num;
+                int currCount = 1 + dp[target - sqNum];
+                mnCount = min(mnCount, currCount);
+            }
+            dp[target] = mnCount;
         }
-       dp[n] = mnCount;
-       return dp[n];
-    }
+        return dp[n];
+    } 
     int numSquares(int n) 
     {
-        vector<int>dp(n + 1, -1);
-        return solve(n, dp);
+        return solve(n);
     }
 };
