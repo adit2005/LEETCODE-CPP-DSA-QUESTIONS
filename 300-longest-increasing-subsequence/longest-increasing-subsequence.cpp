fcb74a1a27ee648632a57745f8eb8bc99cb37570
Aditya singh chauhan <adityaschauhan346@gmail.com>
2023-09-23 20:29:01 +0530

    Time: 494 ms (32.70%) | Memory: 292.8 MB (6.44%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, 1); // Initialize a DP array with all values set to 1.
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                }
+         int n = nums.size();
+        //APPROACH 2
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        for(int ind = n-1;ind>=0;ind--){
+            for(int prev = ind-1;prev>=-1;prev--){
+                
+        int nottake = dp[ind+1][prev+1];
+        int take =0;
+        if(prev==-1 || nums[ind]>nums[prev]){
+            take = 1+dp[ind+1][ind+1];
+        }
+        dp[ind][prev+1]=max(nottake,take);
             }
         }
-
-        // Find the maximum value in the DP array, which represents the length of the LIS.
-        int max_length = 0;
-        for (int i = 0; i < n; i++) {
-            max_length = max(max_length, dp[i]);
-        }
-
-        return max_length;
+        return dp[0][0];
     }
 };
