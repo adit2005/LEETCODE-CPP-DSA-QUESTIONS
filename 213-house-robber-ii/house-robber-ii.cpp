b086a64cffd540e5dd313721918873f63f36a498
Aditya singh chauhan <adityaschauhan346@gmail.com>
2023-09-26 18:45:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (16.90%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
+	int sum(int i,vector<int>& nums){
+		int p1=nums[0],p2;
+		for(int i=1;i<nums.size();i++){
+			int pick=nums[i];
+			if(i!=1)pick+=p2;
+			int notpick=p1;
+			int curri=max(pick,notpick);
+			p2=p1;
+			p1=curri;
+		}
+		return p1;
+	}
+
 	int rob(vector<int>& nums) {
 		int n=nums.size();
 		if(n==1)return nums[0];
-		vector<int>nums1,nums2,dp1(n-1),dp2(n-1);
+		vector<int>nums1,nums2;
 		for(int i=0;i<n;i++){
 			if(i!=0)nums1.push_back(nums[i]);
 			if(i!=n-1)nums2.push_back(nums[i]);
 		}
-		dp1[0]=nums1[0];
-		for(int i=1;i<n-1;i++){
-			int pick=nums1[i];
-			if(i!=1)pick+=dp1[i-2];
-			int notpick=dp1[i-1];
-			dp1[i]=max(pick,notpick);
-		}
-		dp2[0]=nums2[0];
-		for(int i=1;i<n-1;i++){
-			int pick=nums2[i];
-			if(i!=1)pick+=dp2[i-2];
-			int notpick=dp2[i-1];
-			dp2[i]=max(pick,notpick);
-		}
-		return max(dp1[n-2],dp2[n-2]); 
+		return max(sum(n-2,nums1),sum(n-2,nums2)); 
 	}
 };
