4e6d3e00aa3895dbd36f5f24936f918439bfd2f7
Aditya singh chauhan <adityaschauhan346@gmail.com>
2023-09-27 22:19:57 +0530

    Time: 19 ms (37.72%) | Memory: 156.1 MB (6.32%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -5,14 +5,18 @@ public:
     {
         if (n == 1)
             return 0;
-        return calc(1, 1, 1, n);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        return calc(1, 1, 1, n, dp);
     }
-    int calc(int step, int val, int copy, int n)
+    int calc(int step, int val, int copy, int n, vector<vector<int>> &dp)
     {
         if (step > n || val > n)
             return INT_MAX;
         if (val == n)
             return step;
-        return min(calc(step + 1, val + copy, copy, n), calc(step + 2, 2 * val, val, n));
+        if (dp[step][val] != -1)
+            return dp[step][val];
+        dp[step][val] = min(calc(step + 1, val + copy, copy, n, dp), calc(step + 2, 2 * val, val, n, dp));
+        return dp[step][val];
     }
 };
