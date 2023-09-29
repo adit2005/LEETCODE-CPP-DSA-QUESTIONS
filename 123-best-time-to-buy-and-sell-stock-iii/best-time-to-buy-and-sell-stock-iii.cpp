e4c9b04469bbe415a22fb48caaf624423e6a5a96
Aditya singh chauhan <adityaschauhan346@gmail.com>
2023-09-29 21:27:12 +0530

    Time: 161 ms (73.66%) | Memory: 75.6 MB (81.61%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,32 +2,30 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-    // Creating a 3d - dp of size [n+1][2][3] initialized to 0
-    vector<vector<vector<int>>> dp(n+1,
-                                    vector<vector<int>> 
-                                            (2,vector<int>(3,0)));
-                                            
-    // As dp array is intialized to 0, we have already covered the base case
+     vector<vector<int>> ahead(2,vector<int> (3,0));
+    
+    vector<vector<int>> cur(2,vector<int> (3,0));
+    
     
     for(int ind = n-1; ind>=0; ind--){
         for(int buy = 0; buy<=1; buy++){
             for(int cap=1; cap<=2; cap++){
                 
                 if(buy==0){// We can buy the stock
-                    dp[ind][buy][cap] = max(0+dp[ind+1][0][cap], 
-                                -prices[ind] + dp[ind+1][1][cap]);
+                    cur[buy][cap] = max(0+ahead[0][cap], 
+                                -prices[ind] + ahead[1][cap]);
                  }
     
                 if(buy==1){// We can sell the stock
-                    dp[ind][buy][cap] = max(0+dp[ind+1][1][cap],
-                                prices[ind] + dp[ind+1][0][cap-1]);
+                    cur[buy][cap] = max(0+ahead[1][cap],
+                                prices[ind] + ahead[0][cap-1]);
                 }
             }
         }
+        ahead = cur;
     }
     
-    
-    return dp[0][0][2];
-   
+    return ahead[0][2];
+
     }
 };
