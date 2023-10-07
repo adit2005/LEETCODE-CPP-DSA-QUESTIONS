678a8d2c08f0eaad7671acc8b865d50085df1af5
Aditya singh chauhan <adityaschauhan346@gmail.com>
2023-10-07 18:09:25 +0530

    Time: 949 ms (28.62%) | Memory: 10 MB (43.17%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -1,75 +1,41 @@
 class Solution {
 public:
+    int vis[17];
+    int n;
     
-    vector<bool> vis;
-    
-    bool helper(vector<int>& nums, int i, int n, int k, int curr_sum, int target)
-    {
-        // base case
-        
-        if(k == 1)
+    bool solve(vector<int>& nums, int ind, int cs, int target, int k){
+        if(k==1){
             return true;
-        
-        if(i == n)
-            return false;
-        
-        // we found one subset with sum == target, find other k - 1  subsets with sum == target
-        
-        if(curr_sum == target)
-        {
-            return helper(nums, 0, n, k - 1, 0, target);
         }
-        
-        for(int j = i; j < n; j++)
-        {
-            // if curr element is not taken and curr_sum + nums[j] is <= target, then take it and mark it visited
-            
-            if(vis[j] == false && curr_sum + nums[j] <= target)
-            {
-                vis[j] = true;
-                
-                curr_sum += nums[j];
-                
-                if(helper(nums, j + 1, n, k, curr_sum, target))
+        if(ind==n){
+            return false;
+        }
+        if(cs==target)
+            return solve(nums, 0, 0, target, k-1);
+        for(int i=ind;i<n;i++){
+            if (i-1>=0 && nums[i]==nums[i-1] && !vis[i-1]) //handle TLE for consecutive same elements
+                continue;
+            if(!vis[i] && cs+nums[i]<=target){
+                vis[i]=1;
+                if(solve(nums,i+1,cs+nums[i],target,k))
                     return true;
-                
-                // backtrack
-                
-                vis[j] = false;
-                
-                curr_sum -= nums[j];
+                vis[i]=0;
             }
         }
-        
         return false;
     }
     
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        
-        int n = nums.size();
-        
-        int sum = 0;
-        
-        // find the sum of the nums array
-        
-        for(auto x : nums)
-        {
-            sum += x;
-        }
-        
-        // sum is odd
-        
-        if(sum % k)
+        n=nums.size();
+        memset(vis,0,sizeof(vis));
+        int sum=0;
+        for(auto x: nums)
+            sum+=x;
+        if(k==0 || sum%k!=0)
             return false;
-        
-        // req_sum for each subset
-        
-        sum /= k;
-        
-        // take a visited array
-        
-        vis.assign(n, false);
-        
-        return helper(nums, 0, n, k, 0, sum);
+        int target=sum/k;
+        int cs=0;
+        int ind=0;
+        return solve(nums, ind, cs, target, k);
     }
 };
