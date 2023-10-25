bfff5071e1579b2e54c59ff32b131e930bce836e
Aditya singh chauhan <adityaschauhan346@gmail.com>
2023-10-25 13:33:03 +0530

    Time: 2 ms (51.37%) | Memory: 6.5 MB (12.22%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     int kthGrammar(int n, int k) {
-        if(n==1 && k==1) return 0;
-        int middle=pow(2,(n-1))/2;
-        if(k<=middle) return kthGrammar(n-1,k);
-        else return !kthGrammar(n-1,k-middle);
+        bool areValuesSame = true;
+        n = pow(2, n - 1);
+        while (n != 1) {
+            n /= 2;
+            if (k > n) {
+                k -= n;
+                areValuesSame = !areValuesSame;
+            }
+        }
+        return ((areValuesSame) ? 0 : 1);
     }
 };
