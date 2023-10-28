f30e90f34ccd4059be3d2153b32219f777150344
Aditya singh chauhan <adityaschauhan346@gmail.com>
2023-10-28 13:10:27 +0530

    Time: 91 ms (24.96%) | Memory: 18.2 MB (53.28%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.cpp
+++ 1332-count-vowels-permutation/count-vowels-permutation.cpp
@@ -1,19 +1,68 @@
 class Solution {
 public:
-    int countVowelPermutation(int n) {
-        long a = 1, e = 1, i = 1, o = 1, u = 1, mod = pow(10, 9) + 7;
-        long a2, e2, i2, o2, u2;
+    int MOD = 1e9 + 7;
 
-        for(int j = 2; j <= n ; j++){
-            a2 = (e+i+u) % mod;
-            e2 = (a+i) % mod;
-            i2 = (e+o) % mod;
-            o2 = i;
-            u2 = (o+i) % mod;
+    // Recursive function to calculate the count of valid permutations
+    long long int f(int n, int i, char c, vector<vector<int>> &dp, unordered_map<char, int> &mp) {
+        // If we've reached the desired length, return 1 (a valid permutation)
+        if (i == n) return 1;
 
-            a= a2, e = e2, i = i2, o = o2, u  = u2;
+        long long count = 0;
+
+        // If we've already computed the result for this character 'c' and position 'i', return it
+        if (dp[mp[c]][i] != -1) return dp[mp[c]][i] % MOD;
+
+        // Check the allowed characters that can follow the current character 'c'
+        if (c == 'a') {
+            // After 'a', only 'e' is allowed
+            count += f(n, i + 1, 'e', dp, mp);
         }
 
-        return (a+e+i+o+u) %mod;
+        if (c == 'e') {
+            // After 'e', 'a' and 'i' are allowed
+            count += f(n, i + 1, 'a', dp, mp);
+            count += f(n, i + 1, 'i', dp, mp);
+        }
+
+        if (c == 'i') {
+            // After 'i', any character except 'i' is allowed
+            count += f(n, i + 1, 'a', dp, mp);
+            count += f(n, i + 1, 'e', dp, mp);
+            count += f(n, i + 1, 'o', dp, mp);
+            count += f(n, i + 1, 'u', dp, mp);
+        }
+
+        if (c == 'o') {
+            // After 'o', only 'i' and 'u' are allowed
+            count += f(n, i + 1, 'i', dp, mp);
+            count += f(n, i + 1, 'u', dp, mp);
+        }
+
+        if (c == 'u') {
+            // After 'u', only 'a' is allowed
+            count += f(n, i + 1, 'a', dp, mp);
+        }
+
+        // Store the result in the memoization table and return it modulo MOD
+        return dp[mp[c]][i] = count % MOD;
+    }
+
+    int countVowelPermutation(int n) {
+        // Map to convert char to int for using in the memoization table (dp)
+        unordered_map<char, int> mp;
+        mp['a'] = 0;
+        mp['e'] = 1;
+        mp['i'] = 2;
+        mp['o'] = 3;
+        mp['u'] = 4;
+
+        // Initialize a memoization table (dp) with -1 values
+        vector<vector<int>> dp(5, vector<int>(n + 1, -1));
+
+        // Call the recursive function for each starting vowel to consider all possibilities
+        long long cnt = f(n, 1, 'a', dp, mp) + f(n, 1, 'e', dp, mp) + f(n, 1, 'i', dp, mp) + f(n, 1, 'o', dp, mp) + f(n, 1, 'u', dp, mp);
+
+        // Return the total count of valid permutations modulo MOD
+        return cnt % MOD;
     }
 };
