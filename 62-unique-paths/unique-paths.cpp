99fa0038f43131fd84ff4acbecbfc0db176c93fa
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-02-07 01:36:28 +0530

    Time: 4 ms (11.13%) | Memory: 7.8 MB (6.99%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
-    int countWaysUtil(int i , int j , vector<vector<int>>& dp){
-        if(i == 0 && j == 0){
-            return 1;
+    int countWaysUtil(int m , int n , vector<vector<int>>& dp){
+        
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            
+            if (i == 0 && j == 0) {
+                dp[i][j] = 1;
+                continue; 
+            }
+
+            
+            int up = 0;
+            int left = 0;
+
+            
+            if (i > 0)
+                up = dp[i - 1][j];
+
+           
+            if (j > 0)
+                left = dp[i][j - 1];
+
+            
+            dp[i][j] = up + left;
         }
+    }
 
-        if(i <0 || j < 0){
-            return 0;
-        }
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        int up = countWaysUtil(i - 1, j, dp);
-        int left = countWaysUtil(i, j - 1, dp);
-
-        return dp[i][j] = up+left;
+    
+    return dp[m - 1][n - 1];
 
     }
     
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return countWaysUtil(m-1, n-1,dp);
+        return countWaysUtil(m, n,dp);
     }
 };
