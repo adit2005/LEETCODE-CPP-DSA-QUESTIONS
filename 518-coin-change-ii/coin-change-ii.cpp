a64d40070a8d74e2db75d9862107185daa9a96b4
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-02-09 20:54:45 +0530

    Time: 7 ms (80.19%) | Memory: 19.5 MB (32.63%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-int change(int amount, vector<int>& coins) {
-int n = coins.size();
-vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
-for (int i = 0; i <= n; i++) {
-    dp[i][0] = 1;
-}
-for (int i = 0; i <= amount; i++) {
-    dp[0][amount] = 0;
-}
-dp[0][0] = 1;
-for (int coinCount = n - 1; coinCount >= 0; coinCount--) {
-    for (int amt = 1; amt <= amount; amt++) {
-        if (coins[coinCount] > amt) {
-            dp[coinCount][amt] = dp[coinCount + 1][amt];
-        } else {
-            int pick = dp[coinCount][amt - coins[coinCount]];
-            int nonPick = dp[coinCount + 1][amt];
-            dp[coinCount][amt] = pick + nonPick;
-        }
-    }
-}
-return dp[0][amount];
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        int t = amount;
 
-        
+        vector<vector<int>>dp(n, vector<int>(t+1, 0));
+
+       
+        for(int i = 0; i<= t ; i++){
+            if(i % coins[0] == 0 ) {
+                dp[0][i] = 1;
+            }
+        }
+
+        for(int ind = 1; ind < n ; ind++){
+            for(int target = 0; target<= t ; target++){
+                long notTaken = dp[ind-1][target];
+
+                long taken = 0 ;
+                if(coins[ind] <= target){
+                    taken = dp[ind][target-coins[ind]];
+                } 
+
+                dp[ind][target] = notTaken + taken;
+
+            }
+
+
+
+        }
+
+        return dp[n - 1][t];
     }
 };
