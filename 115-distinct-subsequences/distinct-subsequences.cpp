e0332de05b592d62e46c01af9080ce6124a1416f
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-02-13 17:44:56 +0530

    Time: 2259 ms (5.01%) | Memory: 419.1 MB (11.50%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-int dp[1001][1001];
-int solve(string &s,string &t,int ind1,int ind2){
-     if(ind2 == 0) return 1;
-        if(ind1 == 0) return 0;
-        if(dp[ind1][ind2] != -1){
-            return dp[ind1][ind2];
-        }
-        if(s[ind1-1]==t[ind2-1])
-        {
-            return dp[ind1][ind2]= solve(s,t,ind1-1,ind2-1)+solve(s,t,ind1-1,ind2);
+const int prime = 1e9 + 7;
+
+    int count(string s, string t, int ind1, int ind2, vector<vector<int>>& dp) {
+    
+    if (ind2 < 0)
+        return 1;
+    
+   
+    if (ind1 < 0)
+        return 0;
+
+   
+    if (dp[ind1][ind2] != -1)
+        return dp[ind1][ind2];
+
+    int result = 0;
+
+   
+    if (s[ind1] == t[ind2]) {
+        int leaveOne = count(s, t, ind1 - 1, ind2 - 1, dp);
+        int stay = count(s, t, ind1 - 1, ind2, dp);
+
+        result = (leaveOne + stay) % prime;
+    } else {
         
-        }
-         else
-        {
-            return dp[ind1][ind2]= solve(s,t,ind1-1,ind2);
-       
-        }
+        result = count(s, t, ind1 - 1, ind2, dp);
+    }
+
+ 
+    dp[ind1][ind2] = result;
+    return result;
 }
     int numDistinct(string s, string t) {
-        memset(dp,-1,sizeof(dp));
-        return solve(s,t,s.size(),t.size());
+        int l1 = s.size();
+        int l2 = t.size();
+        vector<vector<int>> dp(l1, vector<int>(l2, -1));
+        return count(s, t, l1 - 1, l2 - 1, dp);
     }
 };
