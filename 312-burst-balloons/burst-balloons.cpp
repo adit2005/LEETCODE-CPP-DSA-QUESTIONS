72b71d98a8ddbbee4c5f1591e86db5ced76076e5
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-02-18 16:07:53 +0530

    Time: 105 ms (91.02%) | Memory: 11.9 MB (30.18%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -3,11 +3,11 @@ public:
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
     
-    // Add 1 to the beginning and end of the nums array
+   
     nums.insert(nums.begin(), 1);
     nums.push_back(1);
     
-    // Create a DP array for memoization
+   
     vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
 
     for (int i = n; i >= 1; i--) {
@@ -15,15 +15,15 @@ public:
             if (i > j) continue;
             int maxi = INT_MIN;
             
-            // Iterate through each possible balloon to burst last
+          
             for (int ind = i; ind <= j; ind++) {
-                // Calculate the coins obtained by bursting the ind-th balloon last
+              
                 int coins = nums[i - 1] * nums[ind] * nums[j + 1];
                 
-                // Calculate the maximum coins for the remaining balloons
+               
                 int remainingCoins = dp[i][ind - 1] + dp[ind + 1][j];
                 
-                // Update the maximum coins
+               
                 maxi = max(maxi, coins + remainingCoins);
             }
             
