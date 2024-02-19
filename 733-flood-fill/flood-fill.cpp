e8ac0374a8c5350aeacd0cb3b486d303dde9b4ff
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-02-19 19:46:09 +0530

    Time: 4 ms (85.94%) | Memory: 16.5 MB (41.85%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int m = image.size(), n = image[0].size();
-        DFS(image, sr, sc, m, n, image[sr][sc], newColor);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int  m  = image.size(),n = image[0].size();
+        dfs(image,sr,sc,m,n,image[sr][sc],color);
         return image;
     }
-    
-    void DFS(vector<vector<int>>& image, int r, int c, int m, int n, int target, int newColor){
-        if(r < 0 || c < 0 || r == m || c == n || image[r][c] == newColor || image[r][c] != target) return;
-        image[r][c] = newColor;
-        DFS(image, r + 1, c, m, n, target, newColor);
-        DFS(image, r - 1, c, m, n, target, newColor);
-        DFS(image, r, c + 1, m, n, target, newColor);
-        DFS(image, r, c - 1, m, n, target, newColor);
+
+    void dfs(vector<vector<int>>&image,int r,int c , int m , int n , int target,int color){
+        if(r<0||c<0||r==m||c==n||image[r][c] == color || image[r][c] != target) return;
+        image[r][c] = color;
+        dfs(image,r+1, c, m , n , target,color);
+        dfs(image,r-1,c,m,n,target,color);
+        dfs(image,r,c+1,m,n,target,color);
+        dfs(image,r,c-1,m,n,target,color);
     }
 };
