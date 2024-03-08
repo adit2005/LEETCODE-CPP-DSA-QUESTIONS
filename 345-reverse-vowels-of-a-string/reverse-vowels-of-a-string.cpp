4c8b7011072978d58fc934e015ccc857887693b1
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-03-08 14:29:22 +0530

    Time: 4 ms (82.39%) | Memory: 10 MB (11.37%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        // Convert the input string to a character array.
-        string word = s;
-        int start = 0;
-        int end = s.length() - 1;
-        string vowels = "aeiouAEIOU";
-        
-        // Loop until the start pointer is no longer less than the end pointer.
-        while (start < end) {
-            // Move the start pointer towards the end until it points to a vowel.
-            while (start < end && vowels.find(word[start]) == string::npos) {
-                start++;
+        unordered_set<char> st = {'a','A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'};
+        int i=0, j=s.length()-1;
+        while(i<j){
+            while(!st.count(s[i]) && i<j){
+                i++;
             }
-            
-            // Move the end pointer towards the start until it points to a vowel.
-            while (start < end && vowels.find(word[end]) == string::npos) {
-                end--;
-            }
-            
-            // Swap the vowels found at the start and end positions.
-            swap(word[start], word[end]);
-            
-            // Move the pointers towards each other for the next iteration.
-            start++;
-            end--;
+            while(!st.count(s[j]) && i<j) j--;
+            swap(s[i], s[j]);
+            i++;
+            j--;
         }
-        
-        // Return the modified string.
-        return word;
+        return s;
     }
 };
