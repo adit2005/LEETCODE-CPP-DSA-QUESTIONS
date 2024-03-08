c2921111b6bf57351672aa4522fc665426177256
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-03-08 19:10:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (8.23%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
-    
-    // returns the length of longest common subsequence
-    int isSubs(string& s1, string& s2, int i , int j,vector<vector<int>> &t)
-    {
-        if(i == 0 || j == 0)
-            return 0;
-        if(t[i][j] != -1)
-            return t[i][j];
-        if(s1[i-1] == s2[j-1])
-            return t[i][j] = 1 + isSubs(s1,s2,i-1,j-1,t);
-        else
-            return t[i][j] = isSubs(s1,s2,i,j-1,t);
-    }
-    
-    
-    bool isSubsequence(string s1, string s2) {
-        int m = s1.length();
-        int n = s2.length();
-        
-        // intialising dp matrix with -1
-        
-        if(m >  n)
-            return false;
-        
-        vector<vector<int>> t(m+1,vector<int> (n+1,-1));
-    
-        if(isSubs(s1,s2,m,n,t) >= m)
-            return true;
-        return false;
+    bool isSubsequence(std::string s, std::string t) {
+        int i = 0, j = 0;
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.size();
     }
 };
