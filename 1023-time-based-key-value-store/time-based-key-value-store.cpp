a2062600c8e6ca84cad252f31343ce70cd2fcdc0
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-03-11 00:01:01 +0530

    Time: 286 ms (47.99%) | Memory: 135.9 MB (15.55%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.cpp
+++ 1023-time-based-key-value-store/time-based-key-value-store.cpp
@@ -1,39 +1,19 @@
 class TimeMap {
-private:
-    unordered_map<string, vector<pair<string, int>>> map; // key, {value, timestamp}
-    
-    string searchVal(vector<pair<string, int>> &vec, const int &timestamp) {
-        int low = 0;
-        int high = vec.size() - 1;
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            if (vec[mid].second > timestamp) {
-                high = mid - 1;
-            } else if (vec[mid].second < timestamp) {
-                low = mid + 1;
-            } else {
-                return vec[mid].first;
-            }
-        }
-        
-        return high >= 0 ? vec[high].first : "";
-    }
 public:
+    unordered_map<string,map<int,string>>m;
     TimeMap() {
         
     }
     
     void set(string key, string value, int timestamp) {
-        map[key].push_back({value, timestamp});
+        m[key][timestamp]=value;
     }
     
     string get(string key, int timestamp) {
-        if (map.find(key) == map.end()) {
-            return "";
-        }
-        return searchVal(map[key], timestamp);
+        auto it=m[key].upper_bound(timestamp);
+        if(it==m[key].begin())return "";
+        it--;
+        return it->second;
     }
 };
 
