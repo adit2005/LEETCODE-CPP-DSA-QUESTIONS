ff9fca23c9c24e10a7c41d3b2ea57b8b2bd94978
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-03-13 11:01:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (73.82%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,21 +1,27 @@
-class Solution 
-{
+class Solution {
 public:
-    int pivotInteger(int n) 
-    {
-        
-        int totsum=0;
-        for(int i=1; i<=n; i++) 
-            totsum += i;
-        
-        int presum=0;
-        
-        for(int i=1; i<=n; i++)
-        {
-            presum += i; 
-            if((totsum - presum + i) == presum) 
-                return i;
+    int getSum(int x) {
+        return x * (x + 1) / 2;
+    }
+
+    int pivotInteger(int n) {
+        int sum = getSum(n);
+
+        int l = 1;
+        int r = n;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            int firstPart = getSum(m);
+            int secondPart = sum - firstPart + m; 
+            
+            if (firstPart == secondPart)
+                return m;
+            else if (firstPart > secondPart)
+                r = m - 1;
+            else
+                l = m + 1;
         }
+
         return -1;
     }
 };
