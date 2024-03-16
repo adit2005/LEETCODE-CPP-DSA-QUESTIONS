046ca3a3bc465f2925949a1922b74e2755b0b2ca
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-03-16 13:45:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (48.48%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -5,17 +5,17 @@ public:
         if(n == 1 || n == 2)
             return n;
         
-        vector<int> t(n+1, 0);
+        vector<long long int> t(n+1, 0);
         
         t[1] = 1;
         t[2] = 2;
         t[3] = 5;
         
         for(int i = 4; i<=n; i++) {
-            t[i] = (2*t[i-1]%M + t[i-3]%M) % M;
+            t[i] = (2*t[i-1]%M + t[i-3]%M) ;
         }
         
-        return t[n];
+        return t[n]% M;
         
         
     }
