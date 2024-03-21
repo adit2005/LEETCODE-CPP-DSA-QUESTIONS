a68b5adf10a9c2928ad58b30ec69d132c51275b3
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-03-21 21:59:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (52.97%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int countVowelStrings(int n) {
-        vector<int> dp(5, 1);
-        int ans = 0;
+        int a=1, e=1, i=1, o=1, u=1;
         
         while(--n){
-            for(int i=3; i>=0; i--){
-                dp[i] += dp[i+1];
-            }
+            o += u;
+            i += o;
+            e += i;
+            a += e;
         }
         
-        for(auto x:dp) ans += x;
-        
-        return ans;
+        return a+e+i+o+u;
     }
 };
