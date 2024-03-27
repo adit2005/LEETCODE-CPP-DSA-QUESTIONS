16fb4fcee043de1180231d5873d5f9745d02a302
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-03-28 02:34:37 +0530

    Time: 9 ms (71.63%) | Memory: 15.6 MB (99.47%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
@@ -11,50 +11,30 @@
  */
 class Solution {
 public:
-    
-    map<pair<int, int>, vector<TreeNode*>> mp;
-    
-    vector<TreeNode*> solve(int start, int end) {
-        
-        if(start > end) {
-            return {NULL};
-        }
-        
-        if(start == end) {
-            TreeNode* root = new TreeNode(start);
-            return {root};
-        }
-        
-        if(mp.find({start, end}) != mp.end())
-            return mp[{start, end}];
-        
-        vector<TreeNode*> result;
-        for(int i = start; i <= end; i++) {
-            
-            vector<TreeNode*> leftList  = solve(start, i-1);
-            vector<TreeNode*> rightList = solve(i+1, end);
-            
-            for(TreeNode* leftRoot : leftList) {
-                
-                for(TreeNode* rightRoot : rightList) {
-                    
-                    TreeNode* root = new TreeNode(i);
-                    root->left  = leftRoot;
-                    root->right = rightRoot;
-                    
-                    result.push_back(root);
-                    
-                }
-                
-            }
-            
-        }
-        
-        return mp[{start, end}] = result;
-        
-    }
-    
     vector<TreeNode*> generateTrees(int n) {
-        return solve(1, n);
+        vector<vector<vector<TreeNode*>>> dp(n + 1, vector(n + 1, vector<TreeNode*>(0)));
+        for (int i = 1; i <= n; i++) {
+            dp[i][i].push_back(new TreeNode(i));
+        }
+
+        for (int numberOfNodes = 2; numberOfNodes <= n; numberOfNodes++) {
+            for (int start = 1; start <= n - numberOfNodes + 1; start++) {
+                int end = start + numberOfNodes - 1;
+                for (int i = start; i <= end; i++) {
+                    vector<TreeNode*> leftSubtrees =
+                        i - 1 >= start ? dp[start][i - 1] : vector<TreeNode*>({NULL});
+                    vector<TreeNode*> rightSubtrees =
+                        i + 1 <= end ? dp[i + 1][end] : vector<TreeNode*>({NULL});
+
+                    for (auto left : leftSubtrees) {
+                        for (auto right : rightSubtrees) {
+                            TreeNode* root = new TreeNode(i, left, right);
+                            dp[start][end].push_back(root);
+                        }
+                    }
+                }
+            }
+        }
+        return dp[1][n];
     }
 };
