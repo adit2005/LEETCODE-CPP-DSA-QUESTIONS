e5a911f9ada7ccf7e9ae9192821ac5c253f4750c
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-03-30 17:56:42 +0530

    Time: 68 ms (89.98%) | Memory: 43.1 MB (90.29%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    int subArray(vector<int>& nums, int k) {
-        int n = nums.size();
-        unordered_map<int, int> map;
-        int count = 0, start = 0, end = 0;
-        while (end < n) {
-            map[nums[end]]++;
-            while (map.size() > k) {
-                map[nums[start]]--;
-                if (map[nums[start]] == 0) {
-                    map.erase(nums[start]);
-                }
-                start++;
-            }
-            count += end - start + 1;
-            end++;
-        }
-        return count;
-    }
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return subArray(nums, k) - subArray(nums, k - 1);
+        int n = nums.size();
+        
+        unordered_map<int, int> mp;
+        
+        int i_chota  = 0;
+        int j        = 0;
+        int i_bada   = 0;
+        
+        int result   = 0;
+  
+        while(j < n) {
+            mp[nums[j]]++;
+            
+            while(mp.size() > k) {
+                mp[nums[i_chota]]--;
+                if(mp[nums[i_chota]] == 0) {
+                    mp.erase(nums[i_chota]);
+                }
+                i_chota++;
+                i_bada = i_chota;
+            }
+            
+            while(mp[nums[i_chota]] > 1) {
+                mp[nums[i_chota]]--;
+                i_chota++;
+            }
+            
+            if(mp.size() == k) {
+                result += i_chota - i_bada + 1;
+            }
+            j++;
+        }
+        
+        return result;
     }
 };
