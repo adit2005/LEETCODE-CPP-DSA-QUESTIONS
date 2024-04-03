747c3e427a35ddcb153c124455e4aa9212ba8d62
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-04-03 21:57:57 +0530

    Time: 12 ms (45.08%) | Memory: 18.2 MB (10.30%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.cpp
+++ 1380-number-of-closed-islands/number-of-closed-islands.cpp
@@ -3,11 +3,11 @@ public:
     int closedIsland(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int count = 0;
         vector<vector<bool>> visit(m, vector<bool>(n));
+        int count = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0 && !visit[i][j] && dfs(i, j, m, n, grid, visit)) {
+                if (grid[i][j] == 0 && !visit[i][j] && bfs(i, j, m, n, grid, visit)) {
                     count++;
                 }
             }
@@ -15,24 +15,29 @@ public:
         return count;
     }
 
-    bool dfs(int x, int y, int m, int n, vector<vector<int>>& grid, vector<vector<bool>>& visit) {
-        if (x < 0 || x >= m || y < 0 || y >= n) {
-            return false;
-        }
-        if (grid[x][y] == 1 || visit[x][y]) {
-            return true;
-        }
-
-        visit[x][y] = true;
+    bool bfs(int x, int y, int m, int n, vector<vector<int>>& grid, vector<vector<bool>>& visit) {
+        queue<pair<int, int>> q;
+        q.push({x, y});
+        visit[x][y] = 1;
         bool isClosed = true;
-        vector<int> dirx {0, 1, 0, -1};
-        vector<int> diry {-1, 0, 1, 0};
 
-        for (int i = 0; i < 4; i++) {
-            int r = x + dirx[i];
-            int c = y + diry[i];
-            if (!dfs(r, c, m, n, grid, visit)) {
-                isClosed = false;
+        vector<int> dirx{0, 1, 0, -1};
+        vector<int> diry{-1, 0, 1, 0};
+
+        while (!q.empty()) {
+            x = q.front().first;   
+            y = q.front().second;  
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int r = x + dirx[i];
+                int c = y + diry[i];
+                if (r < 0 || r >= m || c < 0 || c >= n) {
+                    isClosed = false;
+                } else if (grid[r][c] == 0 && !visit[r][c]) {
+                    q.push({r, c});
+                    visit[r][c] = 1;
+                }
             }
         }
 
