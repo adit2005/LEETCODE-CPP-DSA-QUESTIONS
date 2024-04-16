f649251b6419e95fc320a23c7ffcca9bba2789b8
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-04-16 22:56:20 +0530

    Time: 17 ms (60.29%) | Memory: 10.1 MB (89.93%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
-    int helper(vector<int>& piles, vector<vector<int>>& dp, const vector<int>& suffixSum, int i, int M) {
-        if (i == piles.size()) return 0; 
-        if (i + 2 * M >= piles.size()) return suffixSum[i]; 
-        if (dp[i][M] != 0) return dp[i][M];
-        int result = 0;
-        for (int x = 1; x <= 2 * M; ++x) {
-            result = max(result, suffixSum[i] - helper(piles, dp, suffixSum, i + x, max(M, x)));
+    
+    int n;
+    int t[2][101][101]; //t[2][n+1][n+1] - 2*(n+1)*(n+1) ~n^2 * n ~ O(n^3)
+    int solveForAlice(vector<int>& piles, int person, int i, int M) {
+        
+        if(i >= n)
+            return 0;
+        
+        if(t[person][i][M] != -1)
+            return t[person][i][M];
+        
+        int result = (person == 1) ? -1 : INT_MAX;
+        
+        int stones = 0;
+        
+        for(int x = 1; x <= min(2*M, n-i); x++) {
+            
+            stones += piles[i+x-1];
+            
+            if(person == 1) { //Alice
+                result = max(result, stones + solveForAlice(piles, 0, i+x, max(M, x)));
+            } else { //Bob
+                result = min(result, solveForAlice(piles, 1, i+x, max(M, x)));
+            }
+            
         }
-        dp[i][M] = result;
-        return result;
+        
+        return t[person][i][M] = result;
+        
     }
+    
     int stoneGameII(vector<int>& piles) {
-        if (piles.empty()) return 0;
-        vector<vector<int>> dp(piles.size(), vector<int>(piles.size(), 0));
-        vector<int> suffixSum(piles.size());
-        suffixSum[suffixSum.size() - 1] = piles[piles.size() - 1];
-        for (int i = piles.size() - 2; i >= 0; --i) suffixSum[i] = piles[i] + suffixSum[i + 1];
-        return helper(piles, dp, suffixSum, 0, 1);
+        n = piles.size();
+        
+        memset(t, -1, sizeof(t));
+        
+        return solveForAlice(piles, 1, 0, 1);
     }
 };
