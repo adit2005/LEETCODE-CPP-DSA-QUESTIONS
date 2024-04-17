90fcc7f1c2fe32b155e54cc404b33f2e873dba98
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-04-17 12:20:05 +0530

    Time: 10 ms (52.72%) | Memory: 10.6 MB (98.30%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    int maxSum(vector<int>& arr, int k, int dp[], int start) {
-        int N = arr.size();
-        
-        if (start >= N) {
-            return 0;
-        }
-        
-        // Return the already calculated answer.
-        if (dp[start] != -1) {
-            return dp[start];
-        }
-        
-        int currMax = 0, ans = 0;
-        int end = min(N, start + k);
-        for (int i = start; i < end; i++) {
-            currMax = max(currMax, arr[i]);
-            // Store the maximum of all options for the current subarray.
-            ans = max(ans, currMax * (i - start + 1) + maxSum(arr, k, dp, i + 1));
-        }
-
-        // Store the answer to be reused.
-        return dp[start] = ans;
-    }
-    
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        int dp[arr.size()];
-        memset(dp, -1, sizeof(dp));
+        int N = arr.size();
+
+        int dp[N + 1];
+        memset(dp, 0, sizeof(dp));
         
-        return maxSum(arr, k, dp, 0);
+        for (int start = N - 1; start >= 0; start--) {
+            int currMax = 0;
+            int end = min(N, start + k);
+
+            for (int i = start; i < end; i++) {
+                currMax = max(currMax, arr[i]);
+                // Store the maximum of all options for the current subarray.
+                dp[start] = max(dp[start], dp[i + 1] + currMax * (i - start + 1));
+            }
+        }
+        return dp[0];
     }
 };
