26565c17bb73a03c345b2cf98e925f7ecb5c9f97
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-04-22 15:45:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (50.16%) - LeetSync

--- 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.cpp
+++ 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
-   int secondsToRemoveOccurrences(string s) {
-    int seconds = 0, changed = true;
-    while(changed) {
-        changed = false;
-        for (int i = 0; i < s.size() - 1; ++i) {
-            if (s[i] == '0' && s[i + 1] == '1') {
-                swap(s[i], s[i + 1]);
-                ++i;
-                changed = true;
-            }
-        }
-        seconds += changed;
+  int secondsToRemoveOccurrences(string s) {
+    int zeros = 0, seconds = 0;
+    for (int i = 0; i < s.size(); ++i) {
+        zeros += s[i] == '0';
+        if (s[i] == '1' && zeros)
+            seconds = max(seconds + 1, zeros);
     }
     return seconds;
 }
