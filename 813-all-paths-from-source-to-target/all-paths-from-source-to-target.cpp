f94a1efb4fb8a399ab69460713672888af636d30
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-04-24 21:58:53 +0530

    Time: 11 ms (53.81%) | Memory: 19.1 MB (14.39%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    
-    int target;
-    vector<vector<int>> res;
-    vector<int> tmp;
-    void dfs(vector<vector<int>>& graph, int currNode = 0) {
-	    // updating tmp
-        tmp.push_back(currNode);
-		// and either updating res with it if target is met
-        if (currNode == target) res.push_back(tmp);
-		// or callling dfs again recursively
-        else for (int node: graph[currNode]) {
-            dfs(graph, node);
-        }
-        // backtracking with tmp
-		tmp.pop_back();
-    }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        target = graph.size() - 1;
-        dfs(graph);
-        return res;
+        int s=0;
+        int n=graph.size()-1;
+        vector<int>path;
+        path.push_back(s);
+        vector<vector<int>>ans;
+        queue<vector<int>>q;
+        q.push(path);
+        while(!q.empty()){
+            path=q.front();
+            q.pop();
+            int val=path.back();
+            if(val==n){
+                ans.push_back(path);
+            }
+            for(int &i:graph[val]){
+                vector<int>v(path);
+                v.push_back(i);
+                q.push(v);
+            }
+        }
+        return ans;
     }
 };
