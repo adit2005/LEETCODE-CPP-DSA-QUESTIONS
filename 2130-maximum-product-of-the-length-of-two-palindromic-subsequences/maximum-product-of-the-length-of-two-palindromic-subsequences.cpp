4f6e143b7e1d5ddef6dbe4d7f7d749d0293a8932
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-04-25 21:51:17 +0530

    Time: 29 ms (91.96%) | Memory: 8.1 MB (64.13%) - LeetSync

--- 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
+++ 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
-    int result = 0;
-    bool isPalin(string& s){
-        int i = 0;
-        int j = s.length() - 1;
- 
-        while (i < j) {
-            if (s[i] != s[j]) return false;
-            i++;
-            j--;
+    int palSize(string &s, int mask) {
+        int p1 = 0, p2 = s.size(), res = 0;
+        while (p1 <= p2) {
+            if ((mask & (1 << p1)) == 0)
+                ++p1;
+            else if ((mask & (1 << p2)) == 0)
+                --p2;
+            else if (s[p1] != s[p2])
+                return 0;
+            else
+                res += 1 + (p1++ != p2--);
         }
- 
-        return true;
+        return res;
     }
-    
-    void dfs(string& s, int i, string& s1, string& s2){
-        
-        if(i >= s.length()){
-            if(isPalin(s1) && isPalin(s2))
-                result = max(result, (int)s1.length()*(int)s2.length());
-            return;
-        }
-        
-        s1.push_back(s[i]);
-        dfs(s, i+1, s1, s2);
-        s1.pop_back();
-        
-        s2.push_back(s[i]);
-        dfs(s, i+1, s1, s2);
-        s2.pop_back();
-        
-        dfs(s, i+1, s1, s2);
-    }
-    
+
     int maxProduct(string s) {
-        string s1 = "", s2 = "";
-        dfs(s, 0, s1, s2);
-        
-        return result;
+        int dp[4096] = {}; // dp array to store the lengths of palindromic substrings for each mask
+        int res = 0; // result variable to store the maximum product
+        int mask = (1 << s.size()) - 1; // mask representing all positions in the string
+
+        // Calculate the length of the longest palindrome for each possible mask
+        for (int m = 1; m <= mask; ++m)
+            dp[m] = palSize(s, m);
+
+        // Iterate through all possible masks
+        for (int m1 = mask; m1; --m1) {
+            // Iterate through all possible complementary masks
+            for (int m2 = mask ^ m1; m2; m2 = (m2 - 1) & (mask ^ m1)) {
+                // Calculate the product of palindrome lengths for this combination of masks
+                int product = dp[m1] * dp[m2];
+                // Update the result if this product is greater than the current maximum
+                if (product > res)
+                    res = product;
+            }
+        }
+
+        // Return the maximum product of palindrome lengths
+        return res;
     }
 };
