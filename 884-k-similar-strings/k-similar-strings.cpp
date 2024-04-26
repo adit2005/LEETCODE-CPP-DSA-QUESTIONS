8c8a2ba1ca93fa730d53f38948c977537ef068d2
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-04-26 21:44:53 +0530

    Time: 2167 ms (5.08%) | Memory: 20.4 MB (72.10%) - LeetSync

---
+++ 884-k-similar-strings/k-similar-strings.cpp
@@ -0,0 +1,30 @@
+class Solution {  
+public:
+    map<string, int> mp;
+    int solve(string &s1, string &s2, int i, int n) {
+        if(i == n) {
+            return 0;
+        }
+        if(mp.find(s1) != mp.end()) {
+            return mp[s1];
+        }
+        if(s1[i] == s2[i]) {
+            return mp[s1] = (solve(s1, s2, i + 1, n));
+        }
+        int ans = 100;
+        for(int k = i; k < n; ++k) {
+            if(s1[k] == s2[i]) {
+                swap(s1[i], s1[k]);
+                int steps = 1 +solve(s1, s2, i + 1, n);
+                ans = min(ans, steps);
+                swap(s1[i], s1[k]);
+            }
+        }
+        return ans;
+    }
+
+    int kSimilarity(string s1, string s2) {
+        int n = s1.size();
+        return solve(s1, s2, 0, n);
+    }
+};
