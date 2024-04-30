db8e22a295ee84fb4d45784c47e05cf2f49ad41e
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-04-30 16:58:28 +0530

    Time: 255 ms (55.69%) | Memory: 19.6 MB (45.51%) - LeetSync

--- 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.cpp
+++ 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-	// Returns the set bit for a character.
     int getHash(char &c) {
         return 1<<(c-'a');
     }
     long long wonderfulSubstrings(string word) {
-		// Stores prefix parities.
         unordered_map<int,int> count={{0,1}};
+        unordered_set<char> usedChars;
 		int running=0;
         long long result=0;
         for(char &w:word) {
-			// Update the running parity
             running^=getHash(w);
-			
-			// Add counts of substrings with at most character `c` of odd counts.
-			// As seen in observation we need count of prefix parities with opposite parity of character we are considering for odd count.
-			// The rest remains same as we want even count for all other characters.
-			// Here we are checking all possible characters, however it can be obtimized to check characters only present in the input word.
-            for(char c='a';c<='j';c++)
+            
+            // Add used characters to check for odd count.
+            usedChars.insert(w);
+            
+            // Only iterate over characters encountered till now atleast once.
+            // Since only those can possibly be odd in any substring.
+            for(char c:usedChars)
                 result+=count[running^getHash(c)];
-				
-			// Add counts of substrings with all characters with even counts.
-			// As seen in observation we need count of prefix parities with same parities as current running parity.
-            result+=count[running];
-			
-			// Update the counts for next future iterations.
-            count[running]++;
+            result+=count[running]++;
         }
         return result;
     }
