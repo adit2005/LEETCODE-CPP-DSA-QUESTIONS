d56cd1c6bffb52130bc4002b7a04b146b0529d47
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-05-03 22:49:14 +0530

    Time: 30 ms (85.29%) | Memory: 12.2 MB (90.11%) - LeetSync

--- 1220-smallest-sufficient-team/smallest-sufficient-team.cpp
+++ 1220-smallest-sufficient-team/smallest-sufficient-team.cpp
@@ -1,67 +1,37 @@
 class Solution {
 public:
-    
-    int m, n;
-    int target_mask;
-    vector<int> result;
-    vector<vector<int>> t;
-    
-    void solve(vector<int> &people , int idx , vector<int> &temp, int mask ) {
-        
-       if(idx == people.size()) {
-           if(mask == target_mask) {
-               if(result.size() == 0 || result.size() >= temp.size()) {
-                   //vector<int> copy = temp;
-                   result = temp;
-               }
-           }
-           return;
-       }
-        
-        if(t[idx][mask] != -1) {
-            if(t[idx][mask] <= temp.size())
-               return ; 
-        }
-        
-        if(result.size() != 0 && temp.size() >= result.size()) 
-            return; 
-        
-        solve(people , idx + 1 , temp , mask ); // no
-
-        if( (mask | people[idx]) != mask) {
-            temp.push_back(idx);
-            solve(people, idx + 1, temp , mask | people[idx]);
-
-            temp.pop_back();
-            t[idx][mask] = (temp.size() != 0 ) ? temp.size() : -1;
-        }
-
-    }
-    
     vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
-        n = req_skills.size();                                                                                          
-        m = people.size();
-        
-        unordered_map<string, int> skills; //skill -> unique number
-        for (int i = 0; i < n; ++i)
-            skills[req_skills[i]] = i;
-        
-        // represent each person by a single bitmasked number (k'th bit represents if a person has the k'th skill or not)
-        vector<int> people_skill;
-        for (auto &v: people) {
-            int skill_bit = 0;
-            for (string &skill: v)
-                skill_bit |= 1 << skills[skill];
-            
-            people_skill.push_back(skill_bit);
+        int n = people.size(), m = req_skills.size();
+        unordered_map<string, int> skillId;
+        for (int i = 0; i < m; i++) {
+            skillId[req_skills[i]] = i;
         }
-        
-        // required set of skills mask (All set to 1s)
-        target_mask = pow(2, n) - 1;
-
-        t.resize(62, vector<int>(target_mask+1, -1));
-        vector<int> temp;
-        solve(people_skill, 0, temp, 0);
-        return result;
+        vector<int> skillsMaskOfPerson(n);
+        for (int i = 0; i < n; i++) {
+            for (string skill : people[i]) {
+                skillsMaskOfPerson[i] |= 1 << skillId[skill];
+            }
+        }
+        vector<long long> dp(1 << m, (1LL << n) - 1);
+        dp[0] = 0;
+        for (int skillsMask = 1; skillsMask < (1 << m); skillsMask++) {
+            for (int i = 0; i < n; i++) {
+                int smallerSkillsMask = skillsMask & ~skillsMaskOfPerson[i];
+                if (smallerSkillsMask != skillsMask) {
+                    long long peopleMask = dp[smallerSkillsMask] | (1LL << i);
+                    if (__builtin_popcountll(peopleMask) < __builtin_popcountll(dp[skillsMask])) {
+                        dp[skillsMask] = peopleMask;
+                    }
+                }
+            }
+        }
+        long long answerMask = dp[(1 << m) - 1];
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if ((answerMask >> i) & 1) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
     }
 };
