109b26916eb02bf66e029ec845dd02b0082b060c
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-05-06 16:40:14 +0530

    Time: 19 ms (72.47%) | Memory: 7.4 MB (94.93%) - LeetSync

--- 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
@@ -3,14 +3,15 @@ private:
     const int mod=int(1e9+7);
 public:
     int kInversePairs(int n, int k) {
-        vector<vector<int>> dp(n+1,vector<int>(k+1,0));
-        for(int N=0;N<=n;++N) dp[N][0]=1;
+        vector<int> prev(k+1,0),curr(k+1,0);
+        prev[0]=curr[0]=1;
         for(int N=1;N<=n;++N) {
             for(int K=0;K<=k;++K) {
-                dp[N][K] = (dp[N - 1][K] + (K > 0 ? dp[N][K - 1] : 0)) % mod;
-                dp[N][K] = (dp[N][K] + mod - (K >= N ? dp[N-1][K-N] : 0)) % mod;
+                curr[K] = (prev[K] + (K > 0 ? curr[K - 1] : 0)) % mod;
+                curr[K] = (curr[K] + mod - (K >= N ? prev[K-N] : 0)) % mod;
             }
+            prev = curr;
         }
-        return dp[n][k];
+        return curr[k];
     }
 };
