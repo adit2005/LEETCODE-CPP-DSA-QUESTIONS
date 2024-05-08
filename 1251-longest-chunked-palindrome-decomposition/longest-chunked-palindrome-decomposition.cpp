622941c158c867f5cbb71496cfb4eba8bdc16f31
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-05-08 19:18:09 +0530

    Time: 3 ms (73.47%) | Memory: 9.2 MB (63.78%) - LeetSync

---
+++ 1251-longest-chunked-palindrome-decomposition/longest-chunked-palindrome-decomposition.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestDecomposition(string text) {
+        int ans=0,i=0,j=text.size()-1;
+        string s="",t="";
+        
+        while(i<j){
+            s+=text[i];
+            t+=text[j];
+            string rev=t;
+            reverse(rev.begin(),rev.end());
+            
+            if(s==rev){
+                ans++;
+                t="";
+                s="";
+            }
+            
+            i++;
+            j--;
+        }
+        ans*=2;
+        
+        if(i==j || t.size()!=0)ans++;
+        return ans;
+    }
+};
