7cb8ceba85caaeaad19c0f29824407043d469a48
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-05-11 14:07:24 +0530

    Time: 5 ms (31.68%) | Memory: 9.9 MB (46.98%) - LeetSync

--- 1007-numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.cpp
+++ 1007-numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    void dfs(vector<int>& ans, int N, int& K, int num){
-        if(N==0){
+    void rec(int i, vector<int>& ans,int n,int k,int cur, long long num){
+        if(cur==n){
             ans.push_back(num);
-            return ;
+            return;
         }
-        int digit = num%10;
-        if(digit + K <=9){
-            dfs(ans, N-1, K, num*10 + digit + K);            
+        for(int j=0;j<10;j++){
+            if(abs(j-i)!=k) continue;
+            num=num*10+j;
+            rec(j,ans,n,k,++cur,num);
+            num=(num-j) / 10;
+            cur--;
         }
-        // K!=0 is considered, as when K=0 then the above if condition produces same solution as the below condition. (Ex: N=2, K=0)
-        if(digit - K >=0 && K!=0){
-            dfs(ans, N-1, K, num*10 + digit - K);            
-        }
-       
+       // return;
     }
-    vector<int> numsSameConsecDiff(int N, int K) {
-        
+    vector<int> numsSameConsecDiff(int n, int k) {
         vector<int> ans;
-        if(N==1) ans.push_back(0);
-		// First digit will never be 0 except when N=1.
-        for(int i=1;i<=9;i++){
-            dfs(ans, N-1, K, i);
+        for(int i=1;i<10;i++){
+            rec(i,ans,n,k,1,i);
         }
         return ans;
-        
     }
 };
