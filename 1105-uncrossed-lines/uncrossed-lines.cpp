b11b9512011a022b3f2460eec2df57f7e0a1faf0
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-05-15 16:13:25 +0530

    Time: 12 ms (39.43%) | Memory: 15.1 MB (78.37%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.cpp
+++ 1105-uncrossed-lines/uncrossed-lines.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        
-        vector<vector<int>> t(m+1, vector<int>(n+1));
-        
-        //t[i][j] = x; means, Max lines that can be drawn with nums1 of length i and nums2 of length j
-        //So, we will return t[m][n];
-        
-        for(int i = 0; i<m+1; i++) {
-            for(int j = 0; j<n+1; j++) {
-                if(i == 0 || j == 0)
-                    t[i][j] = 0;
-                else if(nums1[i-1] == nums2[j-1]) {
-                    t[i][j] = 1 + t[i-1][j-1];
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        vector<int> dpPrev(n2 + 1);
+
+        for (int i = 1; i <= n1; i++) {
+            vector<int> dp(n2 + 1);
+            for (int j = 1; j <= n2; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[j] = 1 + dpPrev[j - 1];
                 } else {
-                    t[i][j] = max(t[i-1][j], t[i][j-1]);
+                    dp[j] = max(dp[j - 1], dpPrev[j]);
                 }
             }
+            dpPrev = dp; // Update dpPrev with the values of dp for the next iteration
         }
-        
-        return t[m][n];
+
+        return dpPrev[n2]; // Return the value of dp at the end
     }
 };
