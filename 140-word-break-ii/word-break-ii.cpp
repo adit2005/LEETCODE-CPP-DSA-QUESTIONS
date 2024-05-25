e71e02c3166222959b358fe86dd412e9e447519b
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-05-25 19:27:11 +0530

    Time: 4 ms (32.03%) | Memory: 9.4 MB (32.97%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
-    vector<string> result;
-    unordered_set<string> dict;
-
-    void solve(int i, string& currSentence, string &s) {
-        if(i >= s.length()) {
-            result.push_back(currSentence);
-            return;
-        }
-
-        for(int j = i; j < s.length(); j++) {
-
-            string tempWord = s.substr(i, j-i+1);
-
-            if(dict.find(tempWord) != dict.end()) {
-                string origSentence = currSentence;
-                if(!currSentence.empty())
-                    currSentence += " ";
-                
-                currSentence += tempWord;
-
-                solve(j+1, currSentence, s);
-
-                currSentence = origSentence;
-
-            }
-
-        }
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        unordered_map<string, vector<string>> memoization;
+        return dfs(s, wordSet, memoization);
     }
 
-    vector<string> wordBreak(string s, vector<string>& wordDict) {
-        for(string &word : wordDict) {
-            dict.insert(word);
+private:
+    // Depth-first search function to find all possible word break combinations
+    vector<string> dfs(const string& remainingStr,
+                       const unordered_set<string>& wordSet,
+                       unordered_map<string, vector<string>>& memoization) {
+        // Check if result for this substring is already memoized
+        if (memoization.count(remainingStr)) return memoization[remainingStr];
+        // Base case: when the string is empty, return a list containing an
+        // empty string
+        if (remainingStr.empty()) return {""};
+        vector<string> results;
+        for (int i = 1; i <= remainingStr.length(); ++i) {
+            string currentWord = remainingStr.substr(0, i);
+            // If the current substring is a valid word
+            if (wordSet.count(currentWord)) {
+                for (const string& nextWord :
+                     dfs(remainingStr.substr(i), wordSet, memoization)) {
+                    // Append current word and next word with space in between
+                    // if next word exists
+                    results.push_back(currentWord +
+                                      (nextWord.empty() ? "" : " ") + nextWord);
+                }
+            }
         }
-
-        string currSentence = "";
-        solve(0, currSentence, s);
-
-        return result;
+        // Memoize the results for the current substring
+        memoization[remainingStr] = results;
+        return results;
     }
 };
-
