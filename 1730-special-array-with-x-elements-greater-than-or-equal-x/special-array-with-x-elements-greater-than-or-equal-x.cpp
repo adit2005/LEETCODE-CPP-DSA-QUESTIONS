bc464ee04869dbac3de0ad57861be6f079ad70b6
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-05-27 17:12:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (11.26%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,33 +1,17 @@
 class Solution {
-    
-    int getFirstGreaterOrEqual(vector<int>& nums, int val) {
-        int start = 0;
-        int end = nums.size() - 1;
-
-        int index = nums.size();
-        while (start <= end) {
-            int mid = (start + end) / 2;
-
-            if (nums[mid] >= val) {
-                index = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-
-        return index;
-    }
-    
 public:
     int specialArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        
         int N = nums.size();
-        for (int i = 1; i <= N; i++) {
-            int k = getFirstGreaterOrEqual(nums, i);
-            
-            if (N - k == i) {
+        
+        vector<int> freq(N + 1, 0);
+        for (int i = 0; i < N; i++) {
+            freq[min(N, nums[i])]++;
+        }
+        
+        int numGreaterThanOrEqual = 0;
+        for (int i = N; i >= 1; i--) {
+            numGreaterThanOrEqual += freq[i];
+            if (i == numGreaterThanOrEqual) {
                 return i;
             }
         }
