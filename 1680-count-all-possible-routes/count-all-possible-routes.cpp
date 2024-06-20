7370f0fbd0024bbe742e978e3ee9919c07b3a6bf
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-06-20 22:30:49 +0530

    Time: 205 ms (6.38%) | Memory: 14.1 MB (33.13%) - LeetSync

--- 1680-count-all-possible-routes/count-all-possible-routes.cpp
+++ 1680-count-all-possible-routes/count-all-possible-routes.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     const int MOD = 1000000007;
 
-    int helper(const vector<int>& locations, int city, int finish, int remainFuel, vector<vector<int>>& memo) {
-        if (remainFuel < 0) {
-            return 0;
-        }
-        
-        if (memo[city][remainFuel] != -1) {
-            return memo[city][remainFuel];
-        }
-        
-        int total = city == finish ? 1 : 0;
-        
-        for (int nextCity = 0; nextCity < locations.size(); nextCity++) {
-            if (nextCity != city && remainFuel >= abs(locations[nextCity] - locations[city])) {
-                total = (total + helper(locations, nextCity, finish, remainFuel - abs(locations[nextCity] - locations[city]), memo)) % MOD;
-            }
-        }
-        
-        return memo[city][remainFuel] = total;
-    }
-
     int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
         int n = locations.size();
-        vector<vector<int>> memo(n, vector<int>(fuel + 1, -1));
-        return helper(locations, start, finish, fuel, memo);
+        vector<vector<int>> dp(n, vector<int>(fuel + 1, 0));
+
+        // Base case: If the current city is the finish city, there's at least one way to stay there with any fuel
+        for (int f = 0; f <= fuel; ++f) {
+            dp[finish][f] = 1;
+        }
+
+        // Fill the table
+        for (int remainFuel = 0; remainFuel <= fuel; ++remainFuel) {
+            for (int city = 0; city < n; ++city) {
+                for (int nextCity = 0; nextCity < n; ++nextCity) {
+                    if (nextCity != city) {
+                        int cost = abs(locations[nextCity] - locations[city]);
+                        if (remainFuel >= cost) {
+                            dp[city][remainFuel] = (dp[city][remainFuel] + dp[nextCity][remainFuel - cost]) % MOD;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Return the number of ways to go from the start city with all the fuel available
+        return dp[start][fuel];
     }
 };
