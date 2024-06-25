598b8ae45994dc26f540422c4855157132c8266e
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-06-25 13:10:44 +0530

    Time: 85 ms (76.35%) | Memory: 112.1 MB (40.48%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
-        // Tracks the current number of flips
-        int currentFlips = 0;
-        // Tracks the total number of flips
-        int totalFlips = 0;
+        int n = nums.size();
+        // Queue to keep track of flips
+        deque<int> flipQueue;
+        // Current flip state
+        int flipped = 0;
+        // Total number of flips
+        int result = 0;
 
-        for (int i = 0; i < nums.size(); ++i) {
-            // If the window slides out of the range and the leftmost element
-            // is marked as flipped (2), decrement currentFlips
-            if (i >= k && nums[i - k] == 2) {
-                currentFlips--;
+        for (int i = 0; i < n; ++i) {
+            // Remove the effect of the oldest flip if it's out of the current
+            // window
+            if (i >= k) {
+                flipped ^= flipQueue.front();
+                flipQueue.pop_front();
             }
 
-            // Check if the current bit needs to be flipped
-            if ((currentFlips % 2) == nums[i]) {
-                // If flipping would exceed array bounds, return -1
-                if (i + k > nums.size()) {
+            // If the current bit is 0 it needs to be flipped
+            if (flipped == nums[i]) {
+                // If we cannot flip a subarray starting at index i
+                if (i + k > n) {
                     return -1;
                 }
-                // Mark the current bit as flipped
-                nums[i] = 2;
-                currentFlips++;
-                totalFlips++;
+                // Add a flip at this position
+                flipQueue.push_back(1);
+                // Toggle the flipped state
+                flipped ^= 1;
+                // Increment the flip count
+                result += 1;
+            } else {
+                flipQueue.push_back(0);
             }
         }
 
-        return totalFlips;
+        return result;
     }
 };
