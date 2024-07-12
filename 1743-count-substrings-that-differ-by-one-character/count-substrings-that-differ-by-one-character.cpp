fb5d759c84883768b456fe0c50c1c7feb2d01356
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-07-12 19:54:43 +0530

    Time: 4 ms (62.37%) | Memory: 7.7 MB (52.60%) - LeetSync

--- 1743-count-substrings-that-differ-by-one-character/count-substrings-that-differ-by-one-character.cpp
+++ 1743-count-substrings-that-differ-by-one-character/count-substrings-that-differ-by-one-character.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    int countSubstrings(string s, string t) {
-        int n = s.length();
-        int m = t.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1));
-        vector<vector<int>> dp1(n+1,vector<int>(m+1));
-        int ans = 0;
-        
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(s[i-1]==t[j-1])
-                {
-                    dp[i][j] = 1+dp[i-1][j-1];
-                    dp1[i][j] = dp1[i-1][j-1];
-                }
-                else
-                    dp1[i][j] = 1+dp[i-1][j-1];
-                ans += dp1[i][j];
-            }
-        }
-        return ans;
-    }
+    int countSubstrings(string &s, string &t) {
+    int res = 0;
+    int dpl[101][101] = {}, dpr[101][101] = {};
+    for (int i = 1; i <= s.size(); ++i)
+        for (int j = 1; j <= t.size(); ++j)
+            if (s[i - 1] == t[j - 1])
+                dpl[i][j] = 1 + dpl[i - 1][j - 1];
+    for (int i = s.size(); i > 0; --i)
+        for (int j = t.size(); j > 0; --j)
+            if (s[i - 1] == t[j - 1])
+                dpr[i - 1][j - 1] = 1 + dpr[i][j];
+    for (int i = 0; i < s.size(); ++i)
+        for (int j = 0; j < t.size(); ++j)
+            if (s[i] != t[j])
+                res += (dpl[i][j] + 1) * (dpr[i + 1][j + 1] + 1);
+    return res;
+}
 };
