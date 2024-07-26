bb68776ef46f8f1f95fb1eaa04eff0cfa41d2b2e
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-07-26 23:01:24 +0530

    Time: 37 ms (28.35%) | Memory: 30.1 MB (8.84%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int lcs(string& s1, string& s2, int m, int n, vector<vector<int>>& memo) {
-        if (m == 0 || n == 0) {
-            return 0;
+    int lcs(string& s1, string& s2, int m, int n) {
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1));
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    // One of the two strings is empty.
+                    dp[i][j] = 0;
+                } else if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
         }
-        if (memo[m][n] != -1) {
-            return memo[m][n];
-        }
-        if (s1[m - 1] == s2[n - 1]) {
-            return memo[m][n] = 1 + lcs(s1, s2, m - 1, n - 1, memo);
-        }
-        return memo[m][n] = max(lcs(s1, s2, m - 1, n, memo), lcs(s1, s2, m, n - 1, memo));
+
+        return dp[m][n];
     }
 
     int minInsertions(string s) {
         int n = s.length();
         string sReverse = s;
         reverse(sReverse.begin(), sReverse.end());
-        vector<vector<int>> memo(n + 1, vector<int>(n + 1));
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= n; j++) {
-                memo[i][j] = -1;
-            }
-        }
-
-        return n - lcs(s, sReverse, n, n, memo);
+        return n - lcs(s, sReverse, n, n);
     }
 };
