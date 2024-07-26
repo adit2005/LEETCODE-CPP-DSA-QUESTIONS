260cb8e537bbbb6ab7f60e48c588ac9e0579f845
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-07-26 23:14:55 +0530

    Time: 25 ms (72.91%) | Memory: 8 MB (93.78%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int lcs(string& s1, string& s2, int m, int n) {
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1));
+        vector<int> dp(n + 1, 0);
 
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (i == 0 || j == 0) {
-                    // One of the two strings is empty.
-                    dp[i][j] = 0;
-                } else if (s1[i - 1] == s2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+        for (int i = 1; i <= m; i++) {
+            int prev = 0;
+            for (int j = 1; j <= n; j++) {
+                int temp = dp[j];
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[j] = 1 + prev;
                 } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[j] = max(dp[j], dp[j - 1]);
                 }
+                prev = temp;
             }
         }
 
-        return dp[m][n];
+        return dp[n];
     }
 
     int minInsertions(string s) {
