df83296b2abfe803f8db85df20dac24059d448e4
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-08-03 00:00:53 +0530

    Time: 283 ms (71.85%) | Memory: 112.3 MB (80.49%) - LeetSync

--- 2389-design-a-text-editor/design-a-text-editor.cpp
+++ 2389-design-a-text-editor/design-a-text-editor.cpp
@@ -1,41 +1,63 @@
 class TextEditor {
+    stack<char> left;
+    stack<char> right;
 public:
-    string before, after;
-
-    // Constructor
-    TextEditor() {}
-
-    // Appends the given text to the end of the 'before' string
+    TextEditor() {
+        
+    }
+    
     void addText(string text) {
-        before.insert(end(before), begin(text), end(text));
+        for(auto &c : text){
+            left.push(c);
+        }
     }
-
-    // Deletes up to 'k' characters from the left of the cursor
+    
     int deleteText(int k) {
-        k = min(k, (int)before.size()); // Ensures 'k' does not exceed the length of 'before'
-        before.resize(before.size() - k); // Resizes 'before', removing 'k' characters from the end
-        return k; // Returns the number of characters actually deleted
+        int cnt=0;
+        while(!left.empty() and k>0){
+            left.pop();
+            cnt++;
+            k--;
+        }
+        return cnt;
     }
-
-    // Moves the cursor 'k' positions to the left
+    
     string cursorLeft(int k) {
-        while(k-- && !before.empty()) {
-            after.push_back(before.back()); // Moves the last character of 'before' to 'after'
-            before.pop_back(); // Removes the last character from 'before'
+        while(!left.empty() and k>0){
+            char c = left.top();left.pop();
+            right.push(c);
+            k--;
         }
-        return before.substr(before.size() - min((int)before.size(), 10)); // Returns the last 10 characters of 'before'
+		// returning the last min(10, len) characters to the left of the cursor
+        return cursorShiftString();
     }
-
-    // Moves the cursor 'k' positions to the right
+    
     string cursorRight(int k) {
-        while(k-- && !after.empty()) {
-            before.push_back(after.back()); // Moves the last character of 'after' to 'before'
-            after.pop_back(); // Removes the last character from 'after'
+        while(!right.empty() and k>0){
+            char c = right.top();right.pop();
+            left.push(c);
+            k--;
         }
-        return before.substr(before.size() - min((int)before.size(), 10)); // Returns the last 10 characters of 'before'
+		// returning the last min(10, len) characters to the left of the cursor
+        return cursorShiftString();
+    }
+    
+	// function to return the last min(10, len) characters to the left of the cursor
+    string cursorShiftString(){
+        string rtn = "";
+        int cnt=10;
+        while(!left.empty() and cnt>0){
+            char c = left.top();left.pop();
+            rtn += c;
+            cnt--;
+        }
+        reverse(rtn.begin(),rtn.end());
+        for(int i=0;i<rtn.size();i++){
+            left.push(rtn[i]);
+        }
+        return rtn;
     }
 };
-
 /**
  * Your TextEditor object will be instantiated and called as such:
  * TextEditor* obj = new TextEditor();
