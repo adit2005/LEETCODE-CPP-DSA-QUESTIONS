a4af6a1b1ede608d8ea3e46e8cfcb208d7c56509
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-08-09 19:39:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (82.77%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.cpp
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -1,62 +1,61 @@
 class Solution {
 public:
-
-    bool isMagicGrid(vector<vector<int>>& grid, int r, int c) {
-        //uniqueness 1 to 9 and distinct
-        unordered_set<int> st;
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-                int num = grid[r+i][c+j];
-                if(num < 1 || num > 9 || st.count(num)) {
-                    return false;
-                } else {
-                    st.insert(num);
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int ans = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        for (int row = 0; row + 2 < m; row++) {
+            for (int col = 0; col + 2 < n; col++) {
+                if (isMagicSquare(grid, row, col)) {
+                    ans++;
                 }
             }
         }
-
-        //check Sum - Rows and columns
-        int SUM = grid[r][c] + grid[r][c+1] + grid[r][c+2];
-        for(int i = 0; i < 3; i++) {
-            if(grid[r+i][c] + grid[r+i][c+1] + grid[r+i][c+2] != SUM) {
-                return false;
-            }
-
-            if(grid[r][c+i] + grid[r+1][c+i] + grid[r+2][c+i] != SUM) {
-                return false;
-            }
-        }
-
-
-        //diaornal and anti-diagonal
-        if(grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2] != SUM) {
-            return false;
-        }
-
-        if(grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c] != SUM) {
-            return false;
-        }
-        
-
-        return true;
-
-
+        return ans;
     }
 
-    int numMagicSquaresInside(vector<vector<int>>& grid) {
-        int rows = grid.size();
-        int cols = grid.size();
-
-        int count = 0;
-
-        for(int i = 0; i <= rows-3; i++) {
-            for(int j = 0; j <= cols-3; j++) {
-                if(isMagicGrid(grid, i, j)) {
-                    count++;
-                }
+private:
+    bool isMagicSquare(vector<vector<int>>& grid, int row, int col) {
+        bool seen[10] = {false};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int num = grid[row + i][col + j];
+                if (num < 1 || num > 9) return false;
+                if (seen[num]) return false;
+                seen[num] = true;
             }
         }
 
-        return count;
+        // Check if diagonal sums are the same
+        int diagonal1 =
+            grid[row][col] + grid[row + 1][col + 1] + grid[row + 2][col + 2];
+        int diagonal2 =
+            grid[row + 2][col] + grid[row + 1][col + 1] + grid[row][col + 2];
+
+        if (diagonal1 != diagonal2) return false;
+
+        // Check if all row sums are the same as the diagonal sums
+        int row1 = grid[row][col] + grid[row][col + 1] + grid[row][col + 2];
+        int row2 = grid[row + 1][col] + grid[row + 1][col + 1] +
+                   grid[row + 1][col + 2];
+        int row3 = grid[row + 2][col] + grid[row + 2][col + 1] +
+                   grid[row + 2][col + 2];
+
+        if (!(row1 == diagonal1 && row2 == diagonal1 && row3 == diagonal1)) {
+            return false;
+        }
+
+        // Check if all column sums are the same as the diagonal sums
+        int col1 = grid[row][col] + grid[row + 1][col] + grid[row + 2][col];
+        int col2 = grid[row][col + 1] + grid[row + 1][col + 1] +
+                   grid[row + 2][col + 1];
+        int col3 = grid[row][col + 2] + grid[row + 1][col + 2] +
+                   grid[row + 2][col + 2];
+
+        if (!(col1 == diagonal1 && col2 == diagonal1 && col3 == diagonal1)) {
+            return false;
+        }
+
+        return true;
     }
 };
