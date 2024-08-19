38cbe97637fa8336b49d15438f9e76a408b06ab9
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-08-19 17:01:32 +0530

    Time: 8 ms (48.73%) | Memory: 87.1 MB (19.76%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,22 +1,26 @@
-class Solution
-{
+class Solution {
 public:
-    int minSteps(int n)
-    {
-        if (n == 1)
-            return 0;
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return calc(1, 1, 1, n, dp);
+    int n;
+    vector<vector<int>> memo;
+
+    int minSteps(int n) {
+        if (n == 1) return 0;
+        this->n = n;
+
+        memo = vector<vector<int>>(n + 1, vector<int>(n / 2 + 1, 0));
+        return 1 + minStepsHelper(1, 1);
     }
-    int calc(int step, int val, int copy, int n, vector<vector<int>> &dp)
-    {
-        if (step > n || val > n)
-            return INT_MAX;
-        if (val == n)
-            return step;
-        if (dp[step][val] != -1)
-            return dp[step][val];
-        dp[step][val] = min(calc(step + 1, val + copy, copy, n, dp), calc(step + 2, 2 * val, val, n, dp));
-        return dp[step][val];
+
+    int minStepsHelper(int currLen, int pasteLen) {
+        if (currLen == n) return 0;
+        if (currLen > n) return 1000;
+
+        // return result if it has been calculated already
+        if (memo[currLen][pasteLen] != 0) return memo[currLen][pasteLen];
+
+        int opt1 = 1 + minStepsHelper(currLen + pasteLen, pasteLen);
+        int opt2 = 2 + minStepsHelper(currLen * 2, currLen);
+        memo[currLen][pasteLen] = min(opt1, opt2);
+        return memo[currLen][pasteLen];
     }
 };
