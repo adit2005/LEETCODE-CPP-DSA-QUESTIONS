a580c0341f290805da6414e8f03516af37951eb0
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-09-17 21:43:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (72.35%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -3,26 +3,15 @@ public:
     vector<string> uncommonFromSentences(string s1, string s2) {
         // we need to find unique occurences
         string s = s1 + " " + s2;
+        stringstream str(s);
 
-       
+        
 
         // store all words from s
         unordered_map<string,int> um;
-        string tmp="";
-        for(auto& val : s){ // similar to, i=0;i<s.size();i++
-            if(val == ' '){
-                um[tmp]++;
-                tmp = "";
-            }
-            else{
-                tmp += val; // denotes, val = s[i]
-            }
-        }
-        
-        // condition needed if empty string allowed
-        if(tmp != ""){
+        string tmp;
+        while(str>>tmp){
             um[tmp]++;
-            tmp = "";
         }
 
         // check unique means single count
