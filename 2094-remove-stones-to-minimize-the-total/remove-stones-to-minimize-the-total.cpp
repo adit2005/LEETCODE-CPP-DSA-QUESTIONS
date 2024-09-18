b03df714d1111abbf71707b4abfd8d5966156809
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-09-18 19:23:13 +0530

    Time: 95 ms (99.81%) | Memory: 98.1 MB (99.24%) - LeetSync

--- 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
@@ -1,28 +1,22 @@
+// Using Buckets - TC = O(n + k) && SC = O(1)
 class Solution {
 public:
     int minStoneSum(vector<int>& piles, int k) {
-        int n = piles.size();
-        
-        priority_queue<int> pq;
-
-        for(auto &i : piles) {
-            pq.push(i);
+        int freq[10001] = {0};
+        int ans = 0;
+        for(int pile: piles) {
+            ans += pile;
+            freq[pile]++;
         }
-
-        while(k--) {
-            int curr = pq.top();
-            pq.pop();
-
-            pq.push(curr - floor(curr/2));
+        for(int i = 10000; i > 1 && k > 0; i--) {
+            if(freq[i] > 0) {
+                int canTake = min(freq[i], k);
+                k -= canTake;
+                
+                freq[i - i / 2] += canTake;
+                ans -= canTake * (i / 2);
+            }
         }
-
-        int sum = 0;
-        while(!pq.empty()) {
-            sum += pq.top();
-            pq.pop();
-        }
-
-
-        return sum;
+        return ans;
     }
 };
