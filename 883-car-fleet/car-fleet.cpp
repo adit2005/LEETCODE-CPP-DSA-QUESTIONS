b753e2b6363f5fcec520ac3fede094bd07222315
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-09-20 17:11:32 +0530

    Time: 113 ms (93.88%) | Memory: 80.7 MB (88.25%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
-    
-    struct node{
-        int pos;
-        float time;
-    };
-    
-    static bool compare(struct node &a, struct node &b){
-        if(a.pos == b.pos) return a.time > b.time;
-        return a.pos < b.pos;
-    }
-    
-    int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        int n=position.size();
-        
-        vector<node> fleet;
-        for(int i=0;i<n;i++){
-            fleet.push_back({position[i], (target-position[i])/(float)speed[i]});
+    int carFleet(int target, std::vector<int>& position, std::vector<int>& speed) {
+        int n = position.size();
+        vector<pair<int, double>> cars(n);
+        for (int i = 0; i < n; ++i) {
+            cars[i] = {position[i], (double)(target - position[i]) / speed[i]};
         }
-        
-        sort(fleet.begin(),fleet.end(),compare);
-        
-        int c=0;
-        int i=n-1;
-        while(i>=0){
-            c++;
-            float t=fleet[i].time;
-            while(--i>=0 && fleet[i].time <= t) continue; // cars part of same fleet 
+        sort(cars.begin(), cars.end());
+        stack<double> stack;
+        for (int i = n - 1; i >= 0; --i) {
+            if (stack.empty() || cars[i].second > stack.top()) {
+                stack.push(cars[i].second);
+            }
         }
-        
-        return c;
+        return stack.size();
     }
 };
