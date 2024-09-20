c6388a9b1d43b265d371cd54c512da848e23ac52
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-09-20 15:12:30 +0530

    Time: 2 ms (52.08%) | Memory: 7.5 MB (46.88%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/water-and-jug-problem.cpp
@@ -1,6 +1,19 @@
 class Solution
 {
 public:
+    bool dfs(int x, int y, int z, int m, int curr, vector<int> &vis)
+    {
+        if (curr < 0 || curr > m || vis[curr] == 1)
+            return false;
+        if (curr == z)
+            return true;
+        vis[curr] = 1;
+        bool a = dfs(x, y, z, m, curr + x, vis);
+        bool b = dfs(x, y, z, m, curr - x, vis);
+        bool c = dfs(x, y, z, m, curr + y, vis);
+        bool d = dfs(x, y, z, m, curr - y, vis);
+        return a || b || c || d;
+    }
     bool canMeasureWater(int x, int y, int z)
     {
         if (x + y == z || x == z || y == z)
@@ -8,27 +21,7 @@ public:
         if (x + y < z)
             return false;
         int m = x + y; // max capacity
-        vector<int> v = {x, -x, y, -y};
-        queue<int> q;
-        q.push(0);
         vector<int> vis(m + 1, 0);
-        vis[0] = 1;
-        while (!q.empty())
-        {
-            int curr = q.front();
-            q.pop();
-            if (curr == z)
-                return true;
-            for (int i = 0; i < 4; i++)
-            {
-                int k = curr + v[i];
-                if (k >= 0 && k <= m && vis[k] == 0)
-                {
-                    q.push(k);
-                    vis[k] = 1;
-                }
-            }
-        }
-        return false;
+        return dfs(x, y, z, m, 0, vis);
     }
 };
