6fd690773caf79e594695203b9a59a5f0c60ef57
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-09-20 15:08:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (46.88%) - LeetSync

---
+++ 365-water-and-jug-problem/water-and-jug-problem.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    bool canMeasureWater(int x, int y, int z)
+    {
+        if (x + y == z || x == z || y == z)
+            return true;
+        if (x + y < z)
+            return false;
+        int m = x + y; // max capacity
+        vector<int> v = {x, -x, y, -y};
+        queue<int> q;
+        q.push(0);
+        vector<int> vis(m + 1, 0);
+        vis[0] = 1;
+        while (!q.empty())
+        {
+            int curr = q.front();
+            q.pop();
+            if (curr == z)
+                return true;
+            for (int i = 0; i < 4; i++)
+            {
+                int k = curr + v[i];
+                if (k >= 0 && k <= m && vis[k] == 0)
+                {
+                    q.push(k);
+                    vis[k] = 1;
+                }
+            }
+        }
+        return false;
+    }
+};
