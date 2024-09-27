42c0eb4d7243c4ba408d4e85c45eda1cb6490351
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-09-27 14:30:57 +0530

    Time: 57 ms (37.40%) | Memory: 42.9 MB (84.58%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
     int maxTurbulenceSize(vector<int>& arr) {
-         dp.resize(arr.size(),vector<int>(2,-1));
-         int ans = 0;
-         for(int i = 0 ; i < arr.size() ; i++){
-              //int choise1 = longest subarray we can have of the nature bg sml bg sml
-              //int choise2 = longesy subarray we can have of the nature sml bg
-              int choise1 = 1 + fun(i+1,arr,true);   //next should be big
-              int choise2 = 1 + fun(i+1,arr,false);  //next should be small
-              ans = max(ans , max(choise1,choise2));
-         }
-         return ans;
-    }
-    int fun(int idx , vector<int>&arr , bool flag){
-          if(idx >= arr.size()){
-             return 0;
-          }
-          if(dp[idx][flag] != -1){
-            return dp[idx][flag];
-          }
-          if(flag == true){
-               if(arr[idx-1] < arr[idx]){
-                   return dp[idx][flag] = 1 + fun(idx+1,arr,false);
-               }   
-               else return dp[idx][flag] = 0;
-          }
-          else if(flag == false){
-               if(arr[idx-1] > arr[idx]){
-                   return dp[idx][flag] = 1 + fun(idx+1,arr,true);
-               }
-               else return dp[idx][flag] = 0;
-          }
-          return -1;
+    //From the conditions given to be followed 
+    //we have to find the maximum count of one type of pattern whether 
+    //the k is even or whether it is odd.        
+        int ans = 1;
+        int smaller = 1;
+        int larger = 1;
+    //A pattern is alternating itself if at any k(odd) arr[k]>arr[k+1] then
+    //at the next it has to follow arr[k]<arr[k+1] as k will be even.
+    //If the pattern is not followed then it will switch to counting in 
+    //the next pattern
+        for (int i = 0; i < arr.size() - 1; ++i) {
+    //We have taken 2 pointers smaller and larger which helps us in switching
+    //between patterns
+    //and counting the parts of specific pattern
+            smaller = arr[i] < arr[i + 1] ? smaller + 1 : 1;
+            larger = arr[i] > arr[i + 1] ? larger + 1 : 1;
+            
+            ans = max(ans, max(smaller, larger));
+            //Decide which pattern count is high
+            swap(smaller, larger);
+            //Maintaining the alternating condition in the pattern
+        }
+        return ans;
     }
 };
