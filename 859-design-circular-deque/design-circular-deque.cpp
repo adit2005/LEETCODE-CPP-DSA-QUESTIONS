6f7b903101822bb8883bbaf4f9b80166fe42b864
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-09-28 22:02:20 +0530

    Time: 18 ms (76.79%) | Memory: 23 MB (11.65%) - LeetSync

--- 859-design-circular-deque/design-circular-deque.cpp
+++ 859-design-circular-deque/design-circular-deque.cpp
@@ -1,59 +1,86 @@
+struct Node {
+    int val;
+    Node* next;
+    Node* prev;
+    Node(int val, Node* next = NULL, Node* prev = NULL)
+        : val(val), next(next), prev(prev) {}
+};
+
 class MyCircularDeque {
 private:
-    vector<int> queue;
-    int front;
-    int rear;
+    Node* head;
+    Node* rear;
     int size;
     int capacity;
 
 public:
     MyCircularDeque(int k) {
-        queue = vector<int>(k);
+        head = NULL;
+        rear = NULL;
         size = 0;
         capacity = k;
-        front = 0;
-        rear = k - 1;
     }
 
     bool insertFront(int value) {
         if (isFull()) return false;
-        front = (front - 1 + capacity) % capacity;
-        queue[front] = value;
+        if (head == NULL) {
+            head = new Node(value);
+            rear = head;
+        } else {
+            Node* newHead = new Node(value);
+            newHead->next = head;
+            head->prev = newHead;
+            head = newHead;
+        }
         size++;
         return true;
     }
 
     bool insertLast(int value) {
         if (isFull()) return false;
-        rear = (rear + 1) % capacity;
-        queue[rear] = value;
+        if (head == NULL) {
+            head = new Node(value);
+            rear = head;
+        } else {
+            Node* newNode = new Node(value, NULL, rear);
+            rear->next = newNode;
+            rear = newNode;
+        }
         size++;
         return true;
     }
 
     bool deleteFront() {
         if (isEmpty()) return false;
-        front = (front + 1) % capacity;
+        if (size == 1) {
+            head = NULL;
+            rear = NULL;
+        } else {
+            Node* nextNode = head->next;
+            delete head;
+            head = nextNode;
+        }
         size--;
         return true;
     }
 
     bool deleteLast() {
         if (isEmpty()) return false;
-        rear = (rear - 1 + capacity) % capacity;
+        if (size == 1) {
+            head = NULL;
+            rear = NULL;
+        } else {
+            Node* prevNode = rear->prev;
+            delete rear;
+            rear = prevNode;
+        }
         size--;
         return true;
     }
 
-    int getFront() {
-        if (isEmpty()) return -1;
-        return queue[front];
-    }
+    int getFront() { return (isEmpty()) ? -1 : head->val; }
 
-    int getRear() {
-        if (isEmpty()) return -1;
-        return queue[rear];
-    }
+    int getRear() { return (isEmpty()) ? -1 : rear->val; }
 
     bool isEmpty() { return size == 0; }
 
