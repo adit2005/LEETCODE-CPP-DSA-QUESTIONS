602ed6e7b46ca905c7ce1abb6deab469f403c573
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-10-13 17:43:11 +0530

    Time: 38 ms (84.45%) | Memory: 19.5 MB (76.37%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-    class node {
-        public:
-        int data, row, col;
-        node(int d, int r, int c):data(d),row(r),col(c){}
-    };
-
-    struct compare {
-        bool operator()(node* a, node* b){
-            return a->data > b->data;
-        }
-    };
-
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        priority_queue<node*,vector<node*>, compare> minHeap;
-        int mini = INT_MAX, maxi = INT_MIN;
-        //maintain max while processing first element of each array
-        for (int i = 0; i < nums.size(); i++){
-            maxi = max(maxi, nums[i][0]);
-            minHeap.push(new node(nums[i][0],i,0));
-        }
-        mini = minHeap.top()->data;
-        int start = mini, end = maxi;
-        //process all the remaining element by increasing mini
+        //create a min heap (data, (row,col))
+        priority_queue< pair<int,pair<int,int>> , 
+                vector<pair<int,pair<int,int>>>,
+               greater<pair<int,pair<int,int>>> >minHeap;
 
+        int mini = INT_MAX, maxi = INT_MIN;
+
+        //insert first element into minHeap 
+        for(int i = 0; i < nums.size(); i++){
+            minHeap.push({nums[i][0], {i,0}});
+            maxi = max(maxi, nums[i][0]);
+        }
+        mini = minHeap.top().first;
+        int start = mini, end = maxi;
+
+        //process all elements, by inserting min->next into heap
         while(!minHeap.empty()){
-            node* temp = minHeap.top();
+            pair<int,pair<int,int>> top = minHeap.top();
             minHeap.pop();
-            //assign mini
-            mini = temp->data;
-            //update range
-            if (maxi-mini < end-start){
+            //update mini
+            mini = top.first;
+            //if smaller range found update range
+            if (maxi-mini < end-start)
                 start = mini, end = maxi;
-            }
-            int row = temp->row, col = temp->col;
-            //add next element infront of mini if within array
+            
+            //if next element of mini present push it
+            int row = top.second.first, col = top.second.second;
             if (col+1 < nums[row].size()){
-                minHeap.push(new node(nums[row][col+1],row, col+1));
+                minHeap.push({nums[row][col+1],{row,col+1}});
+                //update maxi after pushing new element
                 maxi = max(maxi, nums[row][col+1]);
             }
-            else //one of the array is fully traveresed!!
+            else
                 break;
         }
         return {start,end};
