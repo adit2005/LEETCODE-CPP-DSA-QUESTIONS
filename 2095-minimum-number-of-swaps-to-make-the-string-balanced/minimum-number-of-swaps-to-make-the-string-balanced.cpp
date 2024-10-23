c4ecb93ac61dd0900b2e04239fdcd4964f5228b7
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-10-23 20:53:10 +0530

    Time: 25 ms (99.76%) | Memory: 35.2 MB (22.14%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        int imbalance = 0;   // Tracks the imbalance between opening and closing brackets
-
-        // Traverse each character in the string
-        for (const char& ch : s) {
-            if (ch == ']') {
-                // If a closing bracket is encountered
-                if (imbalance == 0) {
-                    // If no opening bracket is available, we need to "swap" it later
-                    imbalance++;  // Mark this as a new imbalance
-                } else {
-                    // Otherwise, there's an unmatched opening bracket available, balance it
-                    imbalance--;
-                }
-            } else {
-                // If an opening bracket is encountered, increase the imbalance
-                imbalance++;
+        stack<char> stack;
+        int unbalanced = 0;
+        for (int i = 0; i < s.size(); i++) {
+            char ch = s[i];
+            // If an opening bracket is encountered, push it in the stack.
+            if (ch == '[')
+                stack.push(ch);
+            else {
+                // If the stack is not empty, pop it.
+                if (!stack.empty()) stack.pop();
+                // Otherwise increase the count of unbalanced brackets.
+                else
+                    unbalanced++;
             }
         }
-
-        // The final imbalance is divided by 2 (since one swap fixes two unmatched brackets)
-        return (imbalance + 1) / 2;
+        return (unbalanced + 1) / 2;
     }
 };
