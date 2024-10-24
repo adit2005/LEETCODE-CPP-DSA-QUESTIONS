2a8621c2e9ab85be5bc9b7fd910a4fd97574ccc5
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-10-24 22:36:57 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (84.24%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
@@ -9,39 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-    // Function to check if two trees are flip equivalent
+    int c=0;
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        // If both nodes are null, they are equivalent
-        if (!root1 && !root2)
+        if(root1==NULL && root2==NULL){
             return true;
+            cout<<c;
+        }
         
-        // If one of the nodes is null or the values are not equal, they are not equivalent
-        if (!root1 || !root2 || root1->val != root2->val)
+        if((root1 && !root2) || (root2 && !root1)){
             return false;
-        
-        // Recursively check the children of root1 and root2
-        // left1 and right1 are for checking without flipping
-        // left2 and right2 are for checking with flipping
-        bool left1, right1, left2, right2;
-
-        // Check if left child of root1 is equivalent to left child of root2
-        left1 = flipEquiv(root1->left, root2->left);
-        
-        // Check if right child of root1 is equivalent to right child of root2
-        right1 = flipEquiv(root1->right, root2->right);
-        
-        // Check if left child of root1 is equivalent to right child of root2 (flip case)
-        left2 = flipEquiv(root1->left, root2->right);
-        
-        // Check if right child of root1 is equivalent to left child of root2 (flip case)
-        right2 = flipEquiv(root1->right, root2->left);
-
-        // The two trees are flip equivalent if either:
-        // 1. Both corresponding children are equivalent without flipping
-        // 2. Both corresponding children are equivalent with flipping
-        return ((left1 && right1) || (left2 && right2));
+        }
+        if(root1->val!=root2->val){
+            return false;
+        }
+        if(root1->left &&  root2->left && root1->right && root2->right ){
+            if(root1->left->val==root2->right->val && root1->right->val==root2->left->val){
+               TreeNode* temp=root1->right;
+               root1->right=root1->left;
+               root1->left=temp;
+            }
+            c++;
+        }
+        if((root1->left && root2->right && root1->left->val==root2->right->val) || (root2->left && root1->right && root2->left->val==root1->right->val)){
+              TreeNode* temp=root1->right;
+               root1->right=root1->left;
+               root1->left=temp;
+               c++;
+        }
+        return flipEquiv(root1->left,root2->left) && flipEquiv(root1->right,root2->right);
     }
 };
