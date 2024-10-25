3e14ada3fa7c8b2c9dc89d6b97c00a98d5398411
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-10-25 18:17:00 +0530

    Time: 43 ms (96.98%) | Memory: 47.8 MB (75.82%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        // Create a set to store all folder paths for fast lookup
-        unordered_set<string> folderSet(folder.begin(), folder.end());
+        // Sort the folders alphabetically
+        sort(folder.begin(), folder.end());
+
+        // Initialize the result vector and add the first folder
         vector<string> result;
+        result.push_back(folder[0]);
 
-        // Iterate through each folder to check if it's a sub-folder
-        for (string& f : folder) {
-            bool isSubFolder = false;
-            string prefix = f;
+        // Iterate through each folder and check if it's a sub-folder of the
+        // last added folder in the result
+        for (int i = 1; i < folder.size(); i++) {
+            string lastFolder = result.back();
+            lastFolder += '/';
 
-            // Check all prefixes of the current folder path
-            while (!prefix.empty()) {
-                size_t pos = prefix.find_last_of('/');
-                if (pos == string::npos) break;
-
-                // Reduce the prefix to its parent folder
-                prefix = prefix.substr(0, pos);
-
-                // If the parent folder exists in the set, mark as sub-folder
-                if (folderSet.count(prefix)) {
-                    isSubFolder = true;
-                    break;
-                }
-            }
-
-            // If not a sub-folder, add it to the result
-            if (!isSubFolder) {
-                result.push_back(f);
+            // Check if the current folder starts with the last added folder
+            // path
+            if (folder[i].compare(0, lastFolder.size(), lastFolder) != 0) {
+                result.push_back(folder[i]);
             }
         }
 
+        // Return the result containing only non-sub-folders
         return result;
     }
 };
