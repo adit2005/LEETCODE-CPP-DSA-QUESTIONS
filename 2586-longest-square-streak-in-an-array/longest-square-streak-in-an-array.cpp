474df0537f0661d039f7c0d3f2cc3297fd4c0631
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-10-28 18:29:00 +0530

    Time: 161 ms (15.56%) | Memory: 131.4 MB (12.54%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -1,67 +1,42 @@
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        // Sort the array in ascending order
-        sort(nums.begin(), nums.end());
-
-        // Set to keep track of numbers we've already processed
-        set<int> processedNumbers;
-
         int longestStreak = 0;
 
-        // Iterate through each number in the sorted array
-        for (int current : nums) {
-            // Skip if we've already processed this number
-            if (processedNumbers.find(current) != processedNumbers.end())
+        // Set to store unique numbers from input
+        unordered_set<int> uniqueNumbers(nums.begin(), nums.end());
+
+        // Set to mark numbers that have been visited
+        unordered_set<int> visited;
+
+        // Iterate through each number in the input array
+        for (int startNumber : nums) {
+            if (visited.find(startNumber) != visited.end()) {
+                // Skip if the number has already been part of a streak
                 continue;
+            }
 
-            int streak = current;
-            int streakLength = 1;
+            int currentStreak = 0;
+            long long current = startNumber;
 
-            // Continue the streak as long as we can find the square of the
-            // current number
-            while (true) {
-                // Break if the square would be larger than 10^5 (problem
-                // constraint)
-                if ((long long)streak * (long long)streak > 1e5) break;
+            // Continue the streak as long as the next square is in the set
+            while (uniqueNumbers.find((int)current) != uniqueNumbers.end()) {
+                // Mark the current number as visited
+                visited.insert(current);
 
-                // If we find the square, continue the streak
-                if (binarySearch(nums, streak * streak)) {
-                    streak *= streak;
-                    processedNumbers.insert(streak);
-                    streakLength++;
-                } else {
-                    break;
-                }
+                currentStreak++;
+
+                // Stop if the next square would exceed 10^5
+                if (current * current > 1e5) break;
+
+                current *= current;
             }
 
             // Update the longest streak if necessary
-            longestStreak = max(longestStreak, streakLength);
+            longestStreak = max(longestStreak, currentStreak);
         }
 
-        // Return -1 if no valid streak found, otherwise return the longest
-        // streak
+        // Return -1 if no valid streak found, otherwise return longest streak
         return longestStreak < 2 ? -1 : longestStreak;
-    }
-
-private:
-    // Binary search helper function to efficiently find a value in the sorted
-    // array
-    bool binarySearch(vector<int>& nums, int target) {
-        if (target < 0) return false;
-
-        int left = 0;
-        int right = nums.size() - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) return true;
-            if (nums[mid] > target)
-                right = mid - 1;
-            else
-                left = mid + 1;
-        }
-
-        return false;
     }
 };
