3dbd63f8c122f00a3e7abf1b1a113b0473f88ee5
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-11-09 17:04:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (98.95%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
-       long long minEnd(int n, int x) {
-        long long a = x;
-        while (--n)
-            a = (a + 1) | x;
-        return a;
+    using ll=long long;
+    long long minEnd(ll n, ll x) {
+        /*
+            x : 110000010001
+            anything set in x is set in every num
+        */
+        if(n==1) return x;
+        ll res=0,g=0;
+        for(ll bit=0;bit<32;bit++,g++) {
+            while(x>>g&1) g++;
+            if((n-1)>>bit&1) {
+                res|=(1ll<<g);
+            }
+            
+        }
+        return res|x;
     }
 };
