74749ee67af4c7519100585e5789228c36403e24
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-11-23 19:29:38 +0530

    Time: 192 ms (60.00%) | Memory: 56.4 MB (63.41%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -3,36 +3,29 @@ public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
         int m = box.size();
         int n = box[0].size();
-        vector<vector<char>> result(n, vector<char>(m));
-
-        // Create the transpose of the input grid in `result`
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                result[i][j] = box[j][i];
-            }
-        }
-
-        // Reverse each row in the transpose grid to complete the 90° rotation
-        for (int i = 0; i < n; i++) {
-            reverse(result[i].begin(), result[i].end());
-        }
+        vector<vector<char>> result(n, vector<char>(m, '.'));
 
         // Apply gravity to let stones fall to the lowest possible empty cell in
         // each column
-        for (int j = 0; j < m; j++) {
+        for (int i = 0; i < m; i++) {
             int lowestRowWithEmptyCell = n - 1;
-            // Process each cell in column `j` from bottom to top
-            for (int i = n - 1; i >= 0; i--) {
+            // Process each cell in row `i` in reversed order
+            for (int j = n - 1; j >= 0; j--) {
                 // Found a stone - let it fall to the lowest empty cell
-                if (result[i][j] == '#') {
-                    result[i][j] = '.';
-                    result[lowestRowWithEmptyCell][j] = '#';
+                if (box[i][j] == '#') {
+                    // Place it in the correct position in the rotated grid
+                    result[lowestRowWithEmptyCell][m - i - 1] = '#';
+                    // (Optional - already initialized to '.') result[j][m - i -
+                    // 1] = '.';
                     lowestRowWithEmptyCell--;
                 }
                 // Found an obstacle - reset `lowestRowWithEmptyCell` to the row
                 // directly above it
-                if (result[i][j] == '*') {
-                    lowestRowWithEmptyCell = i - 1;
+                if (box[i][j] == '*') {
+                    // Place the obstacle in the correct position in the rotated
+                    // grid
+                    result[j][m - i - 1] = '*';
+                    lowestRowWithEmptyCell = j - 1;
                 }
             }
         }
