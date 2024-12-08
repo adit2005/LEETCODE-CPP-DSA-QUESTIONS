61ceb3e92e3decb153cb152c56bfda1b965ec166
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-12-08 13:20:50 +0530

    Time: 82 ms (58.56%) | Memory: 142.8 MB (44.59%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
-        sort(events.begin(), events.end());
-        vector<vector<int>> dp(events.size(), vector<int>(3, -1));
-        return findEvents(events, 0, 0, dp);
-    }
-    // Recursive function to find the greatest sum for the pairs.
-    int findEvents(vector<vector<int>>& events, int idx, int cnt,
-                   vector<vector<int>>& dp) {
-        if (cnt == 2 || idx >= events.size()) return 0;
-        if (dp[idx][cnt] == -1) {
-            int end = events[idx][1];
-            int lo = idx + 1, hi = events.size() - 1;
-            while (lo < hi) {
-                int mid = lo + ((hi - lo) >> 1);
-                if (events[mid][0] > end)
-                    hi = mid;
-                else
-                    lo = mid + 1;
-            }
-            int include =
-                events[idx][2] + (lo < events.size() && events[lo][0] > end
-                                      ? findEvents(events, lo, cnt + 1, dp)
-                                      : 0);
-            int exclude = findEvents(events, idx + 1, cnt, dp);
-            dp[idx][cnt] = max(include, exclude);
+        vector<array<int, 3>> times;
+        for (auto& e : events) {
+            // 1 denotes start time.
+            times.push_back({e[0], 1, e[2]});
+            // 0 denotes end time.
+            times.push_back({e[1] + 1, 0, e[2]});
         }
-        return dp[idx][cnt];
+        int ans = 0, maxValue = 0;
+        sort(begin(times), end(times));
+        for (auto& timeValue : times) {
+            // If current time is a start time, find maximum sum of maximum end
+            // time till now.
+            if (timeValue[1]) {
+                ans = max(ans, timeValue[2] + maxValue);
+            } else {
+                maxValue = max(maxValue, timeValue[2]);
+            }
+        }
+        return ans;
     }
 };
