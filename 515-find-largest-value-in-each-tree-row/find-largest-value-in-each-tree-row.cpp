96d5baae60f8f8f31349c8b56f80d7ea2dc289ab
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-12-25 13:46:19 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (36.36%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
@@ -9,38 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
+    vector<int> ans;
+    
     vector<int> largestValues(TreeNode* root) {
-        if (root == nullptr) {
-            return vector<int>{};
-        }
-        
-        vector<int> ans;
-        queue<TreeNode*> queue;
-        queue.push(root);
-        
-        while (!queue.empty()) {
-            int currentLength = queue.size();
-            int currMax = INT_MIN;
-            
-            for (int i = 0; i < currentLength; i++) {
-                TreeNode* node = queue.front();
-                queue.pop();
-                currMax = max(currMax, node->val);
-                
-                if (node->left) {
-                    queue.push(node->left);
-                }
-                
-                if (node->right) {
-                    queue.push(node->right);
-                }
-            }
-            
-            ans.push_back(currMax);
-        }
-        
+        dfs(root, 0);
         return ans;
+    }
+    
+    void dfs(TreeNode* node, int depth) {
+        if (node == nullptr) {
+            return;
+        }
+        
+        if (depth == ans.size()) {
+            ans.push_back(node->val);
+        } else {
+            ans[depth] = max(ans[depth], node->val);
+        }
+        
+        dfs(node->left, depth + 1);
+        dfs(node->right, depth + 1);
     }
 };
