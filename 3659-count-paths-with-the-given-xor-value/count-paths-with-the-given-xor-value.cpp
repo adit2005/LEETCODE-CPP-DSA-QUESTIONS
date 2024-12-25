4b0787ec6617cc85cf8c59d490d2ea0e5bddae34
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-12-25 14:20:24 +0530

    Time: 1427 ms (17.52%) | Memory: 490.4 MB (19.25%) - LeetSync

--- 3659-count-paths-with-the-given-xor-value/count-paths-with-the-given-xor-value.cpp
+++ 3659-count-paths-with-the-given-xor-value/count-paths-with-the-given-xor-value.cpp
@@ -2,44 +2,35 @@ class Solution {
 public:
     const int MOD = 1e9 + 7;
 
-    int dfs(int i, int j, int curr, vector<vector<int>>& grid, int k, vector<vector<unordered_map<int, int>>>& memo) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        // Base case: If we reach the bottom-right cell
-        if (i == m - 1 && j == n - 1) {
-            return (curr == k) ? 1 : 0;
-        }
-
-        // Check if result is already computed
-        if (memo[i][j].count(curr)) {
-            return memo[i][j][curr];
-        }
-
-        int paths = 0;
-
-        // Move right
-        if (j + 1 < n) {
-            paths = (paths + dfs(i, j + 1, curr ^ grid[i][j + 1], grid, k, memo)) % MOD;
-        }
-
-        // Move down
-        if (i + 1 < m) {
-            paths = (paths + dfs(i + 1, j, curr ^ grid[i + 1][j], grid, k, memo)) % MOD;
-        }
-
-        // Store result in memo
-        return memo[i][j][curr] = paths;
-    }
-
     int countPathsWithXorValue(vector<vector<int>>& grid, int k) {
         int m = grid.size();
         int n = grid[0].size();
 
-        // Memoization table
-        vector<vector<unordered_map<int, int>>> memo(m, vector<unordered_map<int, int>>(n));
+        // DP table: dp[i][j][xor_val] stores the number of paths ending at (i, j) with XOR value = xor_val
+        vector<vector<unordered_map<int, int>>> dp(m, vector<unordered_map<int, int>>(n));
 
-        // Start DFS from the top-left corner
-        return dfs(0, 0, grid[0][0], grid, k, memo);
+        // Initialize the starting cell
+        dp[0][0][grid[0][0]] = 1;
+
+        // Fill the DP table
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                for (auto& [xor_val, count] : dp[i][j]) {
+                    // Move down
+                    if (i + 1 < m) {
+                        int new_xor = xor_val ^ grid[i + 1][j];
+                        dp[i + 1][j][new_xor] = (dp[i + 1][j][new_xor] + count) % MOD;
+                    }
+                    // Move right
+                    if (j + 1 < n) {
+                        int new_xor = xor_val ^ grid[i][j + 1];
+                        dp[i][j + 1][new_xor] = (dp[i][j + 1][new_xor] + count) % MOD;
+                    }
+                }
+            }
+        }
+
+        // Return the number of paths reaching (m-1, n-1) with XOR value = k
+        return dp[m - 1][n - 1][k];
     }
 };
