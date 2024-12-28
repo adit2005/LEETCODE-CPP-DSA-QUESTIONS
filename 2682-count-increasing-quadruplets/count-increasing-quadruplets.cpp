d2684eab6af98c3f00f859b2fb8e22f4fac3e657
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2024-12-29 03:40:59 +0530

    Time: 575 ms (59.80%) | Memory: 517.2 MB (30.39%) - LeetSync

--- 2682-count-increasing-quadruplets/count-increasing-quadruplets.cpp
+++ 2682-count-increasing-quadruplets/count-increasing-quadruplets.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     long long countQuadruplets(vector<int>& nums) {
-        long long ans=0;
         int n=nums.size();
-        long long prev_small=0;
-        vector<long long> cnt(n,0);
-        for(int i=0 ; i<n  ; i++){
-            prev_small=0;
-            for(int j=0 ; j<i ; j++){
-                if(nums[i]>nums[j]){
-                    prev_small++;
-                    ans+=cnt[j];
-                }
-                else if(nums[i]<nums[j]){
-                    cnt[j]+=prev_small;
+        vector<vector<int>> greaterThan(n,vector<int>(n,0)),lesserThan(n,vector<int>(n,0));
+        // let greaterThan[i][j] represent number of elements in range i to j which are greater than nums[i]
+        
+        // let lesserThan[i][j] represent number of elements in range i to j which are lesser than nums[j];
+        for(int i=0;i<n;i++){
+            for(int j=i+1,greater=0;j<n;j++){
+                greater+=(nums[j]>nums[i]);
+                greaterThan[i][j]=greater;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=i-1,smaller=0;j>=0;j--){
+                smaller+=(nums[j]<nums[i]);
+                lesserThan[j][i]=smaller;
+            }
+        }
+
+        long long res=0;
+        for(int j=1;j<n-2;j++){
+            for(int k=j+1;k<n-1;k++){
+                if(nums[j]>nums[k]){
+                    int poss_i=(lesserThan[0][k]-lesserThan[j][k]);
+                    int poss_l=(greaterThan[j][n-1]-greaterThan[j][k]);
+                    res+=1ll*poss_i*poss_l;
                 }
             }
         }
-        return ans; 
+        return res;
+        
     }
 };
