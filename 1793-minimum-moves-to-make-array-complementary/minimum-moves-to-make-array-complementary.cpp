50c89d28130434716f241687a7608a951910e16d
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-01 20:38:48 +0530

    Time: 4 ms (89.60%) | Memory: 93.4 MB (66.34%) - LeetSync

--- 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
+++ 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
-    int s[200002]; // sweep array
-    int minMoves(vector<int>& nums, int limit) {
-        int n = nums.size();
-        int reach = 0;
-        for (int i = 0; i < n/2; i++) {
-            int small = min(nums[i],nums[n-1-i]);
-            int big = max(nums[i], nums[n-1-i]);
-            reach = max(reach, small+big);
-            // Everything gets default 2 moves, subtract off moves
-            // if we don't need to take it
-            s[1] += 2;
-            s[small+1]--;
-            s[small+big]--;
-            s[small+big+1]++;
-            s[big+limit+1]++;
+    // Function to determine the minimum number of moves required
+    // to make all the sums of pairs in nums be less than or equal to limit
+    int minMoves(std::vector<int>& nums, int limit) {
+        int n = nums.size(); // Number of elements in nums
+        vector<int> delta(limit * 2 + 2, 0); // Array to track the changes in moves
+      
+        // Calculate the change in moves for each pair
+        for (int i = 0; i < n / 2; ++i) {
+            int lower = std::min(nums[i], nums[n - i - 1]); // Lower of the pair
+            int upper = std::max(nums[i], nums[n - i - 1]); // Upper of the pair
+
+            // Increment by two for the worst case (both need to change)
+            delta[2] += 2;
+            delta[limit * 2 + 1] -= 2;
+
+            // Decrement by one as at least one of the numbers need to change
+            delta[lower + 1] -= 1;
+            delta[upper + limit + 1] += 1;
+
+            // No operation needed when the sum equals lower+upper
+            delta[lower + upper] -= 1;
+            delta[lower + upper + 1] += 1;
         }
-        reach = max(reach, limit*2);
-        int ans = INT_MAX;
-        for (int i = 2; i <= reach; i++) {
-            s[i] += s[i-1];
-            ans = min(ans, s[i]);
+      
+        int minMoves = n; // Start with the worst case moves count
+        int currentMoves = 0; // Initialize a counter for the current moves
+      
+        // Accumulate the delta values and find the minimum moves
+        for (int i = 2; i <= limit * 2; ++i) {
+            currentMoves += delta[i];
+            if (minMoves > currentMoves) {
+                minMoves = currentMoves; // Keep track of the minimum moves
+            }
         }
-        return ans;
+      
+        // Return the minimum moves to make all pair sums within the limit
+        return minMoves;
     }
 };
