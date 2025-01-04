22e5fa365671674029b0e25c5b39730169201244
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-04 18:37:53 +0530

    Time: 17 ms (5.30%) | Memory: 24.8 MB (69.70%) - LeetSync

---
+++ 508-most-frequent-subtree-sum/most-frequent-subtree-sum.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    int findTreeSum(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        // Current root's tree's sum.
+        return root -> val + findTreeSum(root -> left) + findTreeSum(root -> right);
+    }
+    
+    void preOrderTraversal(TreeNode* root, unordered_map<int, int>& sumFreq, int& maxFreq) {
+        if (!root) {
+            return;
+        }
+        
+        // Find current node's tree's sum.
+        int currSum = findTreeSum(root);
+        sumFreq[currSum]++;
+        maxFreq = max(maxFreq, sumFreq[currSum]);
+        
+        // Iterate on left and right subtrees and find their sums.
+        preOrderTraversal(root -> left, sumFreq, maxFreq);
+        preOrderTraversal(root -> right, sumFreq, maxFreq);
+    }
+    
+    vector<int> findFrequentTreeSum(TreeNode* root) {
+        unordered_map<int, int> sumFreq;
+        int maxFreq = 0;
+        
+        // Traverse on all nodes one by one, and find it's tree's sum.
+        preOrderTraversal(root, sumFreq, maxFreq);
+        
+        vector<int> maxFreqSums;
+        for(auto& it : sumFreq) {
+            if (it.second == maxFreq) {
+                maxFreqSums.push_back(it.first);
+            }
+        }
+        
+        return maxFreqSums;
+    }
+};
