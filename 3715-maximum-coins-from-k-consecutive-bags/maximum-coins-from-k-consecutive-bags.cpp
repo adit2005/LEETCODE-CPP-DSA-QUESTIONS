74dde62e3ce76a898a69aa36d3196afb23ee3b8e
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-06 22:43:29 +0530

    Time: 101 ms (100.00%) | Memory: 158 MB (100.00%) - LeetSync

--- 3715-maximum-coins-from-k-consecutive-bags/maximum-coins-from-k-consecutive-bags.cpp
+++ 3715-maximum-coins-from-k-consecutive-bags/maximum-coins-from-k-consecutive-bags.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-        long long maximumCoins(vector<vector<int>>& A, int k) {
+    long long maximumCoins(vector<vector<int>>& A, int k) {
         sort(A.begin(), A.end());
         int n = A.size();
 
@@ -18,15 +18,15 @@ public:
             cur -= 1L * (A[i][1] - A[i][0] + 1) * A[i][2];
         }
 
-        // End at A[i][1]
+        // End at A[a][1]
         cur = 0;
-        for (int i = 0, j = 0; i < n; ++i) {
-            cur += 1L * (A[i][1] - A[i][0] + 1) * A[i][2];
-            while (A[j][1] < A[i][1] - k + 1) {
-                cur -= 1L * (A[j][1] - A[j][0] + 1) * A[j][2];
-                j++;
+        for (int a = 0, b = 0; a < n; ++a) {
+            cur += 1L * (A[a][1] - A[a][0] + 1) * A[a][2];
+            while (A[b][1] < A[a][1] - k + 1) {
+                cur -= 1L * (A[b][1] - A[b][0] + 1) * A[b][2];
+                b++;
             }
-            long long part = 1L * max(0, A[i][1] - k - A[j][0] + 1) * A[j][2];
+            long long part = 1L * max(0, A[a][1] - k - A[b][0] + 1) * A[b][2];
             res = max(res, cur - part);
         }
 
