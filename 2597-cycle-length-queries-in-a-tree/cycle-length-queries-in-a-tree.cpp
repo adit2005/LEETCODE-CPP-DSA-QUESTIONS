e9277ca7376f61e454a15b0d32749e3c93456e65
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-10 17:54:02 +0530

    Time: 9 ms (85.71%) | Memory: 69.3 MB (90.06%) - LeetSync

---
+++ 2597-cycle-length-queries-in-a-tree/cycle-length-queries-in-a-tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+       vector<int> cycleLengthQueries(int n, vector<vector<int>>& queries) {
+        int m = queries.size();
+        vector<int> res(m, 1);
+        for (int i = 0; i < m; ++i) {
+            int x = queries[i][0], y = queries[i][1];
+            while (x != y) {
+                if (x > y)
+                    x /= 2;
+                else
+                    y /= 2;
+                res[i]++;
+            }
+        }
+        return res;
+    }
+};
