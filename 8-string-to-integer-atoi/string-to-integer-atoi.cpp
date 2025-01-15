2f088b97b1022623aa5824d4428dabbbf12139a1
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-15 22:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (79.57%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,33 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    int myAtoi(string s) 
-    {
-        int i=0;
-        int sign=1;
-        long ans=0;
-        while(i<s.length() && s[i]==' ')
-            i++;
-        if(s[i]=='-')
-        {
-            sign=-1;
+    int myAtoiRecursive(string &s, int i, int sign, int num) {
+        // Base case: If the index goes out of bounds or the character is non-digit
+        if (i >= s.size() || !isdigit(s[i])) {
+            return num * sign;
+        }
+
+        // Overflow handling
+        if (num > INT_MAX / 10 || (num == INT_MAX / 10 && s[i] > '7')) {
+            return sign == -1 ? INT_MIN : INT_MAX;
+        }
+
+        // Convert current character to integer and update `num`
+        num = num * 10 + (s[i] - '0');
+
+        // Recursive call for the next character
+        return myAtoiRecursive(s, i + 1, sign, num);
+    }
+
+    int myAtoi(string s) {
+        int i = 0, num = 0, sign = 1;
+
+        // Skip leading whitespaces
+        while (i < s.size() && s[i] == ' ') i++;
+
+        // Handle sign
+        if (i < s.size() && (s[i] == '+' || s[i] == '-')) {
+            sign = (s[i] == '-') ? -1 : 1;
             i++;
         }
-        else if(s[i]=='+')
-            i++;
-        while(i<s.length())
-        {
-            if(s[i]>='0' && s[i]<='9')
-            {
-                ans=ans*10+(s[i]-'0');
-                if(ans>INT_MAX && sign==-1)
-                    return INT_MIN;
-                else if(ans>INT_MAX && sign==1)
-                    return INT_MAX;
-                i++;
-            }
-            else
-                return ans*sign;
-        }
-        return (ans*sign);
+
+        // Start recursion to process digits
+        return myAtoiRecursive(s, i, sign, num);
     }
 };
