4708fb1a271bcaf3d517e9ea9950077cf9426259
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-19 16:39:30 +0530

    Time: 27 ms (78.37%) | Memory: 17.1 MB (93.80%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.cpp
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.cpp
@@ -1,52 +1,43 @@
-#define pii pair<int, int>
-#define piii pair<int, pii>
-#define f first
-#define s second
-
 class Solution {
 public:
-    int m,n;
-    bool isValid(int x, int y){
-        return min(x,y) >= 0 && x < m && y < n;
-    }
-
-    int trapRainWater(vector<vector<int>>& matrix) {
-        m = matrix.size();
-        n = matrix[0].size();
-
-        vector<vector<bool>> visited(m+1, vector<bool>(n+1, false));
-        priority_queue<piii, vector<piii>, greater<piii>> pq;
-        int dx[4] = {1, -1, 0, 0};
-        int dy[4] = {0, 0, 1, -1};
-
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                if (i == 0 || i == m-1 || j == 0 || j == n-1){
-                    pq.push({matrix[i][j], {i, j}});
+    int trapRainWater(vector<vector<int>>& mat)
+    {
+        int n=mat.size();
+        int m=mat[0].size();
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0||j==0||i==n-1||j==m-1)
+                {
+                    pq.push({mat[i][j],{i,j}});
+                    vis[i][j]=true;
                 }
             }
         }
-
-        int ans = 0;
-        while(!pq.empty()){
-            int i = pq.top().s.f;
-            int j = pq.top().s.s;
-            int h = pq.top().f;
+        vector<pair<int,int>>d={{-1,0},{1,0},{0,-1},{0,1}};
+    int ans=0;
+        while(pq.size())
+        {
+            auto temp=pq.top();
             pq.pop();
-
-            if (visited[i][j]) continue;
-            visited[i][j] = 1;
-            ans += h - matrix[i][j];
-
-            for (int k = 0; k < 4; k++){
-                int nx = i + dx[k];
-                int ny = j + dy[k];
-                if (isValid(nx, ny) && !visited[nx][ny]){
-                    pq.push({max(h, matrix[nx][ny]), {nx, ny}});
+            int val=temp.first;
+            int x=temp.second.first;
+            int y=temp.second.second;
+            for(auto it:d)
+            {
+                int nx=x+it.first;
+                int ny=y+it.second;
+                if(nx>=0&&ny>=0&&nx<n&&ny<m&&vis[nx][ny]==false)
+                {
+                    vis[nx][ny]=true;
+                    ans+=max(0,val-mat[nx][ny]);
+                    pq.push({max(val,mat[nx][ny]),{nx,ny}});
                 }
             }
         }
-
         return ans;
     }
 };
