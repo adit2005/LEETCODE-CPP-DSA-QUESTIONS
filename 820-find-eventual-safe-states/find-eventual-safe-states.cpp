337d6f01897ea09da3f4055408b4971b71b5910a
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-24 13:43:43 +0530

    Time: 13 ms (67.99%) | Memory: 51.1 MB (99.70%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,46 +1,42 @@
 class Solution {
 public:
+    bool dfs(int node, vector<vector<int>>& adj, vector<bool>& visit,
+             vector<bool>& inStack) {
+        // If the node is already in the stack, we have a cycle.
+        if (inStack[node]) {
+            return true;
+        }
+        if (visit[node]) {
+            return false;
+        }
+        // Mark the current node as visited and part of current recursion stack.
+        visit[node] = true;
+        inStack[node] = true;
+        for (auto neighbor : adj[node]) {
+            if (dfs(neighbor, adj, visit, inStack)) {
+                return true;
+            }
+        }
+        // Remove the node from the stack.
+        inStack[node] = false;
+        return false;
+    }
+
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> indegree(n);
-        vector<vector<int>> adj(n);
+        vector<bool> visit(n), inStack(n);
 
         for (int i = 0; i < n; i++) {
-            for (auto node : graph[i]) {
-                adj[node].push_back(i);
-                indegree[i]++;
-            }
-        }
-
-        queue<int> q;
-        // Push all the nodes with indegree zero in the queue.
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-            }
-        }
-
-        vector<bool> safe(n);
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            safe[node] = true;
-
-            for (auto& neighbor : adj[node]) {
-                // Delete the edge "node -> neighbor".
-                indegree[neighbor]--;
-                if (indegree[neighbor] == 0) {
-                    q.push(neighbor);
-                }
-            }
+            dfs(i, graph, visit, inStack);
         }
 
         vector<int> safeNodes;
         for (int i = 0; i < n; i++) {
-            if (safe[i]) {
+            if (!inStack[i]) {
                 safeNodes.push_back(i);
             }
         }
+
         return safeNodes;
     }
 };
