8674d884ae44f9b0acab34fb2277acc5880baf10
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-26 03:19:28 +0530

    Time: 9 ms (87.39%) | Memory: 24.5 MB (73.87%) - LeetSync

--- 2326-sum-of-scores-of-built-strings/sum-of-scores-of-built-strings.cpp
+++ 2326-sum-of-scores-of-built-strings/sum-of-scores-of-built-strings.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    // Function to compute the LPS (Longest Prefix Suffix) array for the string `s`
-    vector<int> lps(string& s) {
-        vector<int> lps(s.size()); // LPS array to store the length of the longest prefix-suffix
-        for (int i = 1, j = 0; i < s.size(); ++i) {
-            // If there's a mismatch between the current character and the prefix
-            while (j && s[i] != s[j])
-                j = max(0, lps[j - 1]); // Backtrack to the previous valid LPS value
-            
-            // If there's a match, extend the prefix-suffix length
-            j += (s[i] == s[j]);
-            lps[i] = j; // Update the LPS value for the current position
+    vector<int> z(string& s) {
+        vector<int> z(s.size());
+        int l = 0, r = 1;
+        for (int i = 1; i < s.size(); ++i) {
+            z[i] = i > r ? 0 : min(r - i, z[i - l]);
+            while (i + z[i] < s.size() && s[z[i]] == s[z[i] + i])
+                ++z[i];
+            if (z[i] + i > r) {
+                l = i;
+                r = z[i] + i;
+            }
         }
-        return lps;
+        return z;
     }
-
-    // Function to compute the sum of scores of all prefixes of the string `s`
     long long sumScores(string s) {
-        vector<int> cnt; // To store the cumulative sum of valid LPS values
-        for (int j : lps(s)) {
-            // If `j` (LPS value) is 0, the score is 0; otherwise, extend the previous score
-            cnt.push_back(j == 0 ? 0 : cnt[j - 1] + 1);
-        }
-        
-        // The total score is the sum of `cnt` values plus the size of the string
-        // (the length of the entire string contributes to the score of itself as a prefix)
+        vector<int> cnt = z(s);
         return accumulate(begin(cnt), end(cnt), 0LL) + s.size();
     }
 };
