180eb1ef9326e697fb530af9234dcff8bccb767c
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-26 19:35:34 +0530

    Time: 173 ms (5.88%) | Memory: 120.3 MB (5.88%) - LeetSync

--- 3721-count-mentions-per-user/count-mentions-per-user.cpp
+++ 3721-count-mentions-per-user/count-mentions-per-user.cpp
@@ -1,62 +1,63 @@
-#include <iostream>
-#include <vector>
-#include <sstream>
-#include <algorithm>
-
-using namespace std;
-
+ bool cmp(vector<string>a, vector<string>b){
+        int timeA = stoi(a[1]);
+        int timeB = stoi(b[1]);
+        return timeA == timeB ? a[0] > b[0] : timeA < timeB;
+    }
 class Solution {
 public:
-    void handleMessage(vector<string> event, vector<int>& mentions, vector<int>& offlineTime) {
+    
+
+    void handleMessage(vector<string>event, vector<int>&mentions, vector<int>&offlineTime){
+
         int timestamp = stoi(event[1]);
-        vector<string> ids;
-        
-        // Collecting all ids separated by space
+
+        vector<string>ids;
+        //collecting all ids separated by space
         stringstream s(event[2]);
         string word;
-        while (s >> word) {
+        while(s >> word){
             ids.push_back(word);
         }
 
-        for (string id : ids) {
-            if (id == "ALL") {
-                for (int i = 0; i < mentions.size(); i++) {
+        for(string id: ids){
+            if(id == "ALL"){
+                for(int i = 0; i<mentions.size(); i++){
                     mentions[i]++;
                 }
-            } else if (id == "HERE") {
-                for (int i = 0; i < mentions.size(); i++) {
-                    if (offlineTime[i] == 0 || offlineTime[i] + 60 <= timestamp) {
+            }
+            else if(id == "HERE"){
+                for(int i = 0; i<mentions.size(); i++){
+                    if(offlineTime[i] == 0 || offlineTime[i] + 60 <= timestamp){
                         mentions[i]++;
                     }
                 }
-            } else {
-                int idNum = stoi(id.substr(2)); // Extract user ID from "idX"
+            }
+            else{
+                int idNum = stoi(id.substr(2)); //id1 me se idNum ko 1 assign kar rahe hai
                 mentions[idNum]++;
             }
         }
+
     }
 
-    void handleOffline(vector<string> event, vector<int>& mentions, vector<int>& offlineTime) {
+    void handleOffline(vector<string>event, vector<int>&mentions, vector<int>&offlineTime){
+        
         int timestamp = stoi(event[1]);
         int id = stoi(event[2]);
+
         offlineTime[id] = timestamp;
     }
-
     vector<int> countMentions(int numberOfUsers, vector<vector<string>>& events) {
-        vector<int> mentions(numberOfUsers);
-        vector<int> offlineTime(numberOfUsers);
+        vector<int>mentions(numberOfUsers);
+        vector<int>offlineTime(numberOfUsers);
 
-        // Use a lambda function for sorting
-        sort(events.begin(), events.end(), [](const vector<string>& a, const vector<string>& b) {
-            int timeA = stoi(a[1]);
-            int timeB = stoi(b[1]);
-            return timeA == timeB ? a[0] > b[0] : timeA < timeB;
-        });
+        sort(events.begin(), events.end(), cmp);
 
-        for (vector<string> event : events) {
-            if (event[0] == "MESSAGE") {
+        for(vector<string>event: events){
+            if(event[0] == "MESSAGE"){
                 handleMessage(event, mentions, offlineTime);
-            } else if (event[0] == "OFFLINE") {
+            }
+            else if(event[0] == "OFFLINE"){
                 handleOffline(event, mentions, offlineTime);
             }
         }
