ea4658f7c7ecc92c45ebee32e4ba61a5141b3b52
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-27 19:59:36 +0530

    Time: 291 ms (11.02%) | Memory: 77 MB (26.75%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
+    // Performs DFS and returns true if there's a path between src and target
+    // and false otherwise.
+    bool isPrerequisite(unordered_map<int, vector<int>>& adjList,
+                        vector<bool>& visited, int src, int target) {
+        visited[src] = 1;
+
+        if (src == target) {
+            return true;
+        }
+
+        int answer = false;
+        for (auto adj : adjList[src]) {
+            if (!visited[adj]) {
+                answer =
+                    answer || isPrerequisite(adjList, visited, adj, target);
+            }
+        }
+        return answer;
+    }
+
     vector<bool> checkIfPrerequisite(int numCourses,
                                      vector<vector<int>>& prerequisites,
                                      vector<vector<int>>& queries) {
-        vector<vector<bool>> isPrerequisite(numCourses,
-                                            vector<bool>(numCourses, false));
+        unordered_map<int, vector<int>> adjList;
         for (auto edge : prerequisites) {
-            isPrerequisite[edge[0]][edge[1]] = true;
-        }
-
-        for (int intermediate = 0; intermediate < numCourses; intermediate++) {
-            for (int src = 0; src < numCourses; src++) {
-                for (int target = 0; target < numCourses; target++) {
-                    // If src -> intermediate & intermediate -> target exists
-                    // then src -> target will also exist.
-                    isPrerequisite[src][target] =
-                        isPrerequisite[src][target] ||
-                        (isPrerequisite[src][intermediate] &&
-                         isPrerequisite[intermediate][target]);
-                }
-            }
+            adjList[edge[0]].push_back(edge[1]);
         }
 
         vector<bool> answer;
         for (auto q : queries) {
-            answer.push_back(isPrerequisite[q[0]][q[1]]);
+            // Reset the visited array for each query.
+            vector<bool> visited(numCourses, false);
+            answer.push_back(isPrerequisite(adjList, visited, q[0], q[1]));
         }
 
         return answer;
