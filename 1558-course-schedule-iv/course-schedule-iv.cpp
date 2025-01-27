254b63b3dc188131925786661cff65428d806f78
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-27 20:02:11 +0530

    Time: 60 ms (49.76%) | Memory: 75.4 MB (32.31%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    // Performs DFS and returns true if there's a path between src and target
-    // and false otherwise.
-    bool isPrerequisite(unordered_map<int, vector<int>>& adjList,
-                        vector<bool>& visited, int src, int target) {
-        visited[src] = 1;
+    // Iterate over each node and perform BFS starting from it.
+    // Mark the starting node as a prerequisite to all the nodes in the BFS
+    // traversal.
+    void preprocess(int numCourses, vector<vector<int>>& prerequisites,
+                    unordered_map<int, vector<int>>& adjList,
+                    vector<vector<bool>>& isPrerequisite) {
+        for (int i = 0; i < numCourses; i++) {
+            queue<int> q;
+            q.push(i);
 
-        if (src == target) {
-            return true;
-        }
+            while (!q.empty()) {
+                int node = q.front();
+                q.pop();
 
-        int answer = false;
-        for (auto adj : adjList[src]) {
-            if (!visited[adj]) {
-                answer =
-                    answer || isPrerequisite(adjList, visited, adj, target);
+                for (auto adj : adjList[node]) {
+                    // If we have marked i as a prerequisite of adj it implies
+                    // we have visited it. This is equivalent to using a visited
+                    // array.
+                    if (!isPrerequisite[i][adj]) {
+                        isPrerequisite[i][adj] = true;
+                        q.push(adj);
+                    }
+                }
             }
         }
-        return answer;
     }
 
     vector<bool> checkIfPrerequisite(int numCourses,
@@ -28,11 +35,14 @@ public:
             adjList[edge[0]].push_back(edge[1]);
         }
 
+        vector<vector<bool>> isPrerequisite(numCourses,
+                                            vector<bool>(numCourses, false));
+        // Store the prerequisite for each node in the array.
+        preprocess(numCourses, prerequisites, adjList, isPrerequisite);
+
         vector<bool> answer;
         for (auto q : queries) {
-            // Reset the visited array for each query.
-            vector<bool> visited(numCourses, false);
-            answer.push_back(isPrerequisite(adjList, visited, q[0], q[1]));
+            answer.push_back(isPrerequisite[q[0]][q[1]]);
         }
 
         return answer;
