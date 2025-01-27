134e6199289d0cea1e33f5fd2d2b17b7ab8b1c61
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-28 01:30:57 +0530

    Time: 63 ms (61.56%) | Memory: 60.2 MB (93.50%) - LeetSync

--- 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
@@ -1,21 +1,12 @@
-class NumStrMinComparator {
-public:
-    bool operator() (string &b, string &a) { // Please note, I swap position of `a` and `b` here!
-        if (a.size() != b.size()) return a.size() < b.size();
-        return  a < b;
-    }
-};
-
 class Solution {
 public:
+    static bool comp(string& a, string& b) {
+        if (a.size() != b.size()) return a.size() < b.size();
+        return a < b;
+    }
+    
     string kthLargestNumber(vector<string>& nums, int k) {
-        priority_queue<string, vector<string>, NumStrMinComparator> minHeap;
-        for (string& num : nums) {
-            minHeap.push(num);
-            if (minHeap.size() > k) {
-                minHeap.pop();
-            }
-        }
-        return minHeap.top();
+        sort(nums.begin(), nums.end(), comp);
+        return nums[nums.size()-k];
     }
 };
