84a9ee2d9d87dfc740873e5adcc48d6f6f41d759
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-29 18:00:11 +0530

    Time: 4 ms (27.53%) | Memory: 14.4 MB (18.65%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,38 +1,41 @@
 class Solution {
-public:
-    vector<int> parents;
-    int find_parent(int a){
-        if(parents[a] == a){
-            return a;
-        } else {
-            return parents[a] = find_parent(parents[a]);    //Finding parents using path compression
+private:
+    // Performs DFS and returns true if there's a path between src and target.
+    bool isConnected(int src, int target, vector<bool>& visited,
+                     vector<int> adjList[]) {
+        visited[src] = true;
+
+        if (src == target) {
+            return true;
         }
-    }
-    void do_union(int node1 , int node2){   
-        node1 = find_parent(node1);
-        node2 = find_parent(node2);
-        if(node1 == node2){     // If they have same parents, do nothing
-            return;
-        } else {                //Else, connect node1 to node2 by assigning the parent of node1 to node2
-            parents[node2] = node1;
-            return;
-        }
-    }
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        parents = vector<int>(1001);
-        for(int i = 0 ; i < 1001 ; ++i){    //Self initializing the parents, because every node is disconnected at first.
-            parents[i] = i;
-        }
-        vector<int> answer(2 , 0);
-        for(int i = 0 ; i < edges.size() ; ++i){
-            if(find_parent(edges[i][0]) == find_parent(edges[i][1])){   //If parents of these nodes are same, means they already visited/connected, hence contributing in cycle.
-                answer[0] = edges[i][0];
-                answer[1] = edges[i][1];
-                break;
-            } else {
-                do_union(edges[i][0] , edges[i][1]);
+
+        int isFound = false;
+        for (int adj : adjList[src]) {
+            if (!visited[adj]) {
+                isFound = isFound || isConnected(adj, target, visited, adjList);
             }
         }
-        return answer;
+
+        return isFound;
+    }
+
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int N = edges.size();
+
+        vector<int> adjList[N];
+        for (auto edge : edges) {
+            vector<bool> visited(N, false);
+
+            // If DFS returns true, we will return the edge.
+            if (isConnected(edge[0] - 1, edge[1] - 1, visited, adjList)) {
+                return edge;
+            }
+
+            adjList[edge[0] - 1].push_back(edge[1] - 1);
+            adjList[edge[1] - 1].push_back(edge[0] - 1);
+        }
+
+        return {};
     }
 };
