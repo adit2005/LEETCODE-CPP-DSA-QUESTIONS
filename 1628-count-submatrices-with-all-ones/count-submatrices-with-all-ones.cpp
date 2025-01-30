6399a8067f00f76847ecfc8fb8f77a2ce7c8a58d
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-31 04:01:23 +0530

    Time: 8 ms (74.13%) | Memory: 22.3 MB (9.78%) - LeetSync

--- 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
@@ -1,29 +1,49 @@
+#include <vector>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
 class Solution {
 public:
     int numSubmat(vector<vector<int>>& mat) {
         int M = mat.size(), N = mat[0].size();
         int res = 0;
+        vector<int> h(N, 0);
 
-        for (int up = 0; up < M; ++up) {
-            vector<int> h(N, 1);
-            for (int down = up; down < M; ++down) {
-                for (int k = 0; k < N; ++k) 
-                    h[k] &= mat[down][k];
-                
-                res += countOneRow(h);
+        for (int i = 0; i < M; ++i) {
+            for (int j = 0; j < N; ++j) {
+                h[j] = (mat[i][j] == 0 ? 0 : h[j] + 1);
             }
+            res += helper(h);
         }
 
         return res;
     }
 
 private:
-    int countOneRow(vector<int>& A) {
-        int res = 0, length = 0;
-        for (int i = 0; i < A.size(); ++i) {
-            length = (A[i] == 0 ? 0 : length + 1);
-            res += length;
+    int helper(vector<int>& A) {
+        int n = A.size();
+        vector<int> sum(n, 0);
+        stack<int> st;
+
+        for (int i = 0; i < n; ++i) {
+            while (!st.empty() && A[st.top()] >= A[i]) 
+                st.pop();
+
+            if (!st.empty()) {
+                int preIndex = st.top();
+                sum[i] = sum[preIndex] + A[i] * (i - preIndex);
+            } else {
+                sum[i] = A[i] * (i + 1);
+            }
+
+            st.push(i);
         }
+
+        int res = 0;
+        for (int s : sum) res += s;
+        
         return res;
     }
 };
