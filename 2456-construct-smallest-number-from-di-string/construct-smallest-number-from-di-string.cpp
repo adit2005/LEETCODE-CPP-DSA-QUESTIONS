38dd2bd4835bf2246d76ae78febcc6b523ed1ac9
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-01-30 23:16:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (84.24%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     string smallestNumber(string s) {
-        string res, stack;
-        for (int i = 0; i <= s.length(); i++) {
-            stack.push_back('1' + i);
-            if (i == s.length() || s[i] == 'I') {
-                while (!stack.empty()) {
-                    res.push_back(stack.back());
-                    stack.pop_back();
-                }
+        string res;
+        int n = s.size(), j = 0;
+        for (int i = 0; i <= s.size(); ++i) {
+            res.push_back('1' + i);
+            if (i == n || s[i] == 'I') {
+                reverse(res.begin() + j, res.end());
+                j = i + 1;
             }
         }
         return res;
