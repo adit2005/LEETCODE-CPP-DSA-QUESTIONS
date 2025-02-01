a9f41942be2fc33d1af5cfd452fc14903038184d
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-02-01 14:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (86.97%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -4,7 +4,7 @@ public:
         // Iterate through indexes 0 to n - 1
         for (int index = 0; index < nums.size() - 1; index++) {
             // Compare the parities of the current and next number
-            if (nums[index] % 2 == nums[index + 1] % 2) {
+            if ((nums[index] & 1) ^ (nums[index + 1] & 1) == 0) {
                 // If the two adjacent numbers have the same parity, return
                 // false
                 return false;
