794fe7a329873e0af0fa62d555794ab615c94e31
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-02-03 03:29:57 +0530

    Time: 769 ms (5.19%) | Memory: 309.4 MB (5.19%) - LeetSync

--- 2591-frog-jump-ii/frog-jump-ii.cpp
+++ 2591-frog-jump-ii/frog-jump-ii.cpp
@@ -1,9 +1,39 @@
 class Solution {
 public:
+    bool works(vector<int>& stones, int k) {
+        int cur = 0;
+        vector<int> newstones;
+        set<int> chosen;
+        while (cur + k < stones.back()) {
+            auto it = upper_bound(stones.begin(), stones.end(), cur + k);
+            it--;
+            if (*it == cur)
+                return false;
+            cur = *it;
+            chosen.insert(cur);
+        }
+        for (int i : stones) {
+            if (chosen.count(i) == 0)
+                newstones.push_back(i);
+        }
+        assert((int)newstones.size() >= 2);
+        for (int i = 1; i < newstones.size(); i++) {
+            if (newstones[i] - newstones[i - 1] > k)
+                return false;
+        }
+        return true;
+    }
+
     int maxJump(vector<int>& stones) {
-        int res = stones[1]-stones[0]; // store max difference
-        for(int i = 3; i < stones.size(); i+=2) res = max(res, stones[i]-stones[i-2]); // odd path
-        for(int i = 2; i < stones.size(); i+=2) res = max(res, stones[i]-stones[i-2]); // even path
-        return res;
+        int lo = 0, hi = stones.back();
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (works(stones, mid))
+                hi = mid;
+            else
+                lo = mid + 1;
+        }
+      //  assert(lo == hi);
+        return lo;
     }
 };
