dc46cc20e442688758bceb8ff3cd77b547258330
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-02-04 17:01:33 +0530

    Time: 21 ms (85.96%) | Memory: 98.6 MB (85.00%) - LeetSync

--- 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -1,30 +1,57 @@
 class Solution {
 public:
-    int answer = -1;
-    void dfs(int node, vector<int>& edges, unordered_map<int, int>& dist,
-             vector<bool>& visit) {
-        visit[node] = true;
-        int neighbor = edges[node];
-
-        if (neighbor != -1 && !visit[neighbor]) {
-            dist[neighbor] = dist[node] + 1;
-            dfs(neighbor, edges, dist, visit);
-        } else if (neighbor != -1 && dist.count(neighbor)) {
-            answer = max(answer, dist[node] - dist[neighbor] + 1);
-        }
-    }
-
     int longestCycle(vector<int>& edges) {
         int n = edges.size();
-        vector<bool> visit(n);
-
+        vector<int> indegree(n, 0);
         for (int i = 0; i < n; i++) {
-            if (!visit[i]) {
-                unordered_map<int, int> dist;
-                dist[i] = 1;
-                dfs(i, edges, dist, visit);
+            int val = edges[i];
+            if (val != -1) {
+                indegree[val]++;
             }
         }
-        return answer;
+
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        // Remove all nodes which will never be the part of a cycle and this can
+        // be done using toposort
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            int val = edges[node];
+            if (val != -1) {
+                indegree[val]--;
+                if (indegree[val] == 0)
+                    q.push(val);
+            }
+        }
+
+        // After removing the nodes, we can have multiple cycle, so find the
+        // length of maximum cycle
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] >= 1) {
+                ans = max(ans, dfs(indegree, edges, i));
+            }
+        }
+
+        if (ans == 0)
+            return -1;
+        else
+            return ans + 1;
+    }
+
+    int dfs(vector<int>& indegree, vector<int>& edges, int i) {
+        indegree[i]--;
+        int val = edges[i];
+        int cnt = 0;
+        if (val != -1 && indegree[val] >= 1) {
+            cnt = 1 + dfs(indegree, edges, val);
+        }
+
+        return cnt;
     }
 };
