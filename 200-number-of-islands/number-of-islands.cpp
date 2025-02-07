8e4f76a2fa43c2f17cbb3c17ab5bbdb61ae16c05
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-02-08 01:21:17 +0530

    Time: 34 ms (23.96%) | Memory: 18.4 MB (42.63%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,33 +1,45 @@
- class Solution {
-public:   
-    void dfs(int r, int c, vector<vector<char>>& grid)
-    {
-        if(r<0 || c<0 || r>=grid.size() || c>=grid[0].size() || grid[r][c]=='0')
-        {
-            return ;
-        }      
-        grid[r][c]='0';
-        dfs(r+1,c,grid);
-        dfs(r-1,c,grid);
-        dfs(r,c+1,grid);
-        dfs(r,c-1,grid);
-    }
-    
+class Solution {
+public:
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int ans=0;        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]=='1')
-                {
-                    ans++;
-                    dfs(i,j,grid);
+        if (grid.empty() || grid[0].empty()) {
+            return 0;
+        }
+        
+        int numIslands = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
+        queue<pair<int, int>> q;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    q.push({i, j});
+                    
+                    while (!q.empty()) {
+                        auto [x, y] = q.front();
+                        q.pop();
+                        
+                        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != '1') {
+                            continue;
+                        }
+                        
+                        grid[x][y] = '0'; // mark as visited
+                        
+                        for (auto& dir : directions) {
+                            int nx = x + dir.first;
+                            int ny = y + dir.second;
+                            if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == '1') {
+                                q.push({nx, ny});
+                            }
+                        }
+                    }
                 }
             }
         }
-        return ans;
+        
+        return numIslands;
     }
 };
