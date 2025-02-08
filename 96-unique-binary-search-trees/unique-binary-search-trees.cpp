24109873359a04e48bd13be1c3dc32573ea315f2
Aditya Singh Chauhan <adityaschauhan346@gmail.com>
2025-02-09 02:18:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (36.42%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
+
+
+int func(int n , vector<int>&dp){
+    if(n<=1) return 1;
+    if(dp[n] != -1) return dp[n]; //because this is already visited 
+    int count =0;
+
+    for(int i =1 ;i<=n;i++){
+        count += func(i-1,dp)* func(n-i ,dp);
+    }
+
+      return dp[n] = count;
+}
     int numTrees(int n) {
-        vector<int>result(n+1,0); 
-        result[1]=result[0]=1;
-        for(int i=2;i<=n;i++)
-        {
-            for(int j=0;j<i;j++)
-            {
-                result[i]+=result[j]*result[i-j-1]; 
-            }
-        }
-        return result[n]; 
+     vector<int>dp(n+1, -1);
+     return func(n ,dp);
+
+      
     }
 };
